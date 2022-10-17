28deb223bb4cb57d0633a6a29ba290ccdf43cbc0
Lucis <luciannojunior@gmail.com>
2022-10-17 20:45:08 -0300

    Adding setInterval for banners

--- islands/VideoCarousel.tsx
+++ islands/VideoCarousel.tsx
@@ -1,21 +1,22 @@
 import { JSONSchema7 } from "json-schema";
 import { signal } from "@preact/signals";
 import type { h } from "preact";
+import { useEffect } from "preact/hooks";
 
 export const schema: JSONSchema7 = {
   title: "Carousel de Vídeos",
-  "type": "object",
+  type: "object",
   required: ["video1"],
   properties: {
     video1: {
-      "type": "object",
+      type: "object",
       properties: {
         mobile: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Mobile",
         },
         desktop: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Desktop",
         },
         alt: {
@@ -23,20 +24,20 @@ export const schema: JSONSchema7 = {
           title: "Texto alternativo",
         },
         link: {
-          "type": "string",
+          type: "string",
           title: "Link",
         },
       },
     },
     video2: {
-      "type": "object",
+      type: "object",
       properties: {
         mobile: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Mobile",
         },
         desktop: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Desktop",
         },
         alt: {
@@ -44,20 +45,20 @@ export const schema: JSONSchema7 = {
           title: "Texto alternativo",
         },
         link: {
-          "type": "string",
+          type: "string",
           title: "Link",
         },
       },
     },
     video3: {
-      "type": "object",
+      type: "object",
       properties: {
         mobile: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Mobile",
         },
         desktop: {
-          "type": "string",
+          type: "string",
           title: "Vídeo Desktop",
         },
         alt: {
@@ -65,7 +66,7 @@ export const schema: JSONSchema7 = {
           title: "Texto alternativo",
         },
         link: {
-          "type": "string",
+          type: "string",
           title: "Link",
         },
       },
@@ -81,9 +82,14 @@ interface VideoProps
   link: string;
 }
 
-function VideoLink(
-  { desktop, mobile, alt, link, loading, decoding }: VideoProps,
-) {
+function VideoLink({
+  desktop,
+  mobile,
+  alt,
+  link,
+  loading,
+  decoding,
+}: VideoProps) {
   return (
     <a href={link} class="h-fit">
       <div class="min-h-[465px]">
@@ -98,10 +104,7 @@ function VideoLink(
           loading={loading}
           decoding={decoding}
         >
-          <source
-            src={mobile}
-            type="video/mp4"
-          />
+          <source src={mobile} type="video/mp4" />
         </video>
         <video
           autoPlay
@@ -114,10 +117,7 @@ function VideoLink(
           loading={loading}
           decoding={decoding}
         >
-          <source
-            src={desktop}
-            type="video/mp4"
-          />
+          <source src={desktop} type="video/mp4" />
         </video>
       </div>
     </a>
@@ -139,13 +139,22 @@ const videoDefault = {
 
 const selectedCarousel = signal(0);
 
-export default function VideoCarousel(
-  { video1 = videoDefault, video2, video3 }: Props,
-) {
+export default function VideoCarousel({
+  video1 = videoDefault,
+  video2,
+  video3,
+}: Props) {
   let quantity = 0;
   if (video1) quantity += 1;
   if (video2) quantity += 1;
   if (video3) quantity += 1;
+
+  useEffect(() => {
+    setInterval(() => {
+      const newValue = (selectedCarousel.value + 1) % quantity;
+      selectedCarousel.value = newValue;
+    }, 3_000);
+  }, []);
 
   return (
     <div class="relative w-full overflow-hidden">
@@ -154,7 +163,7 @@ export default function VideoCarousel(
           (100 / quantity) * selectedCarousel.value
         }%]`}
       >
-        <VideoLink {...video1} decoding="async" loading="eager"/>
+        <VideoLink {...video1} decoding="async" loading="eager" />
         {video2 && <VideoLink {...video2} loading="lazy" decoding="async" />}
         {video3 && <VideoLink {...video3} loading="lazy" decoding="async" />}
       </div>
@@ -163,25 +172,22 @@ export default function VideoCarousel(
         <button
           type="button"
           class="w-3 h-3 rounded-full bg-white dark:bg-gray-800"
-          onClick={() => selectedCarousel.value = 0}
-        >
-        </button>
+          onClick={() => (selectedCarousel.value = 0)}
+        ></button>
 
         {video2 && (
           <button
             type="button"
             class="w-3 h-3 rounded-full bg-white dark:bg-gray-800 hover:bg-white dark:hover:bg-gray-800"
-            onClick={() => selectedCarousel.value = 1}
-          >
-          </button>
+            onClick={() => (selectedCarousel.value = 1)}
+          ></button>
         )}
         {video3 && (
           <button
             type="button"
             class="w-3 h-3 rounded-full bg-white dark:bg-gray-800 hover:bg-white dark:hover:bg-gray-800"
-            onClick={() => selectedCarousel.value = 2}
-          >
-          </button>
+            onClick={() => (selectedCarousel.value = 2)}
+          ></button>
         )}
       </div>
     </div>
