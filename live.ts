0858e1da3ca6e0900c488f887f8c023bb681d932
Tiago Gimenes <tlgimenes@gmail.com>
2022-11-21 12:17:24 -0300

    upgrade live (#13)

--- live.ts
+++ live.ts
@@ -1,14 +1,11 @@
-import manifest from "./deco.gen.ts";
-import { start } from "$live/server.ts";
+import manifest from "./fresh.gen.ts";
+import { start } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 import prefetchPlugin from "prefetch";
 import partytownPlugin from "partytown/mod.ts";
 
 await start(manifest, {
-  siteId: 239,
-  site: "fashion",
-  domains: [],
   plugins: [
     partytownPlugin(),
     prefetchPlugin(),
---
+++ routes/_middleware.ts
@@ -0,0 +1,7 @@
+import { withLive } from "$live/live.ts";
+
+export const handler = withLive({
+  siteId: 239,
+  site: "fashion",
+  domains: [],
+});
