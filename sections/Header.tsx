c4908675067847e29bd55b7255cd8274212062d7
Tiago Gimenes <tlgimenes@gmail.com>
2022-11-28 10:58:13 -0300

    improve seo

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -23,23 +23,23 @@ function Navbar() {
   return (
     <div>
       <section class="md:hidden flex p-2 justify-between items-center px-12">
-        <button aria-label="open menu">
+        <button aria-label="open menu" class="p-2">
           <Bars3Icon className="w-8 h-8" />
         </button>
-        <a href="#" class="mr-6" aria-label="search">
+        <a href="#" class="p-2" aria-label="search">
           <MagnifyingGlassIcon className="w-8 h-8" />
         </a>
-        <a href="/" aria-label="home link">
-          <DecoFashionLogo classes="w-40" />
+        <a href="/" aria-label="home link" class="flex-grow">
+          <DecoFashionLogo />
         </a>
-        <a href="#" aria-label="my account">
-          <UserIcon className="w-6 h-6" />
+        <a href="#" class="p-2" aria-label="my account">
+          <UserIcon className="w-8 h-8" />
         </a>
         <Minicart />
       </section>
       <section class="hidden md:flex bg-white flex-row h-[80px] justify-between md:border-b border-[#d3d5db]">
         <div class="flex items-center justify-center flex-1 min-w-[200px]">
-          <a href="/" aria-label="home linkn">
+          <a href="/" aria-label="home link">
             <DecoFashionLogo classes="w-60" />
           </a>
         </div>
