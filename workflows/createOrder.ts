73223e2f033ae429dfdffc016eb24c35f514ccf3
Marcos Candeia <marrcooos@gmail.com>
2023-01-19 18:51:43 -0300

    Remove live dependency
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- workflows/createOrder.ts
+++ workflows/createOrder.ts
@@ -1,6 +1,5 @@
 import { delay } from "https://deno.land/std@0.160.0/async/delay.ts";
 import { WorkflowContext } from "https://raw.githubusercontent.com/mcandeia/deno-workflows/durable/context.ts";
-import { WorkflowFunction } from "https://raw.githubusercontent.com/deco-cx/live/workflows/std/types.ts";
 
 interface OrderForm {
   items: string[];
@@ -20,15 +19,14 @@ export interface Props {
   vtexAccountName: string;
 }
 
-const createOrder: WorkflowFunction<Props, { id: string }, [OrderForm]> =
-  function* createOrder(
-    ctx: WorkflowContext,
-    { vtexAccountName }: Props,
-    orderForm: OrderForm,
-  ) {
-    yield ctx.callActivity(createOrderVtex, vtexAccountName, orderForm);
-    const orderCreated: Order = yield ctx.waitForSignal("order_created");
-    return { id: orderCreated.id };
-  };
+const createOrder = function* createOrder(
+  ctx: WorkflowContext,
+  { vtexAccountName }: Props,
+  orderForm: OrderForm,
+) {
+  yield ctx.callActivity(createOrderVtex, vtexAccountName, orderForm);
+  const orderCreated: Order = yield ctx.waitForSignal("order_created");
+  return { id: orderCreated.id };
+};
 
 export default createOrder;
