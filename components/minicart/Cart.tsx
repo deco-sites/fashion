efe8c1ff95af66158db63c5d659947f73a4bcc61
gimenes <tlgimenes@gmail.com>
2023-02-01 17:50:52 -0300

    add coupon button to cart

--- components/minicart/Cart.tsx
+++ components/minicart/Cart.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "preact/hooks";
+import { useSignal } from "@preact/signals";
 import { useCart } from "../../sdk/cart/useCart.ts";
 import CartItem, { formatPrice } from "./CartItem.tsx";
 
@@ -5,6 +7,75 @@ import Button from "../ui/Button.tsx";
 
 const CHECKOUT_URL =
   "https://bravtexfashionstore.vtexcommercestable.com.br/checkout";
+
+function Coupon() {
+  const { cart, loading, addCouponsToCart } = useCart();
+  const ref = useRef<HTMLInputElement>(null);
+  const displayInput = useSignal(false);
+  const coupon = cart.value?.marketingData.coupon;
+
+  const onApply = () => {
+    const text = ref.current?.value;
+
+    if (typeof text === "string") {
+      addCouponsToCart({ text });
+    }
+  };
+
+  return (
+    <>
+      <div class="flex justify-between items-center text-lg font-medium">
+        <p>
+          Coupon {coupon ? ":" : ""} <span class="font-semibold">{coupon}</span>
+        </p>
+        {!displayInput.value && (
+          <Button
+            variant="tertiary"
+            onClick={() => {
+              displayInput.value = true;
+            }}
+          >
+            <span class="underline">
+              {coupon ? "Alterar Coupon" : "Adicionar Coupon"}
+            </span>
+          </Button>
+        )}
+        {displayInput.value && (
+          <div class="flex gap-2">
+            <Button
+              variant="secondary"
+              disabled={loading.value}
+              onClick={() => {
+                displayInput.value = false;
+              }}
+            >
+              <span>Cancelar</span>
+            </Button>
+            <Button
+              loading={loading.value}
+              variant="primary"
+              onClick={onApply}
+            >
+              <span>Aplicar</span>
+            </Button>
+          </div>
+        )}
+      </div>
+      {displayInput.value && (
+        <div class="flex justify-between items-center gap-2">
+          <input
+            ref={ref}
+            class="flex-grow border rounded p-2"
+            type="text"
+            value={coupon ?? ""}
+            placeholder={"Entre com o seu coupon"}
+          >
+          </input>
+        </div>
+      )}
+    </>
+  );
+}
 
 function Cart() {
   const { cart, loading } = useCart();
@@ -32,11 +103,12 @@ function Cart() {
       </div>
       <div class="pt-4 flex flex-col gap-2">
         {cart.value.totalizers.map(({ name, value }) => (
-          <div class="flex justify-between text-lg font-medium">
+          <div class="flex justify-between items-center text-lg font-medium">
             <p>{name}</p>
             <p>{formatPrice(value)}</p>
           </div>
         ))}
+        <Coupon />
         <p class="text-sm text-gray-500">
           Taxas e frete serāo calculados no checkout
         </p>
