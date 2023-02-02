ab667820120ee987cd3117f7557b85462127e4b9
Lucis <luciannojunior@gmail.com>
2023-02-02 18:46:42 -0300

    Fixing silly mistakes

--- functions/occProductDetailsPage.ts
+++ functions/occProductDetailsPage.ts
@@ -1,38 +1,40 @@
 import { toProductPage } from "$live/std/commerce/occ/transform.ts";
-import type { LoaderFunction } from "$live/std/types.ts";
 import type { ProductDetailsPage } from "$live/std/commerce/types.ts";
+import type { LoaderFunction } from "$live/std/types.ts";
 
 import { occ } from "../clients/instances.ts";
 
-// const DEFAULT_SKU = 1023372;
+const DEFAULT_SKU = "tenis-soho-carmuca-masc-areia-66957-72";
 
 /**
  * @title Oracle Commerce Cloud Product Page Loader
  * @description Works on routes of type /:slug/p
  */
-const productPageLoader: LoaderFunction<null, ProductDetailsPage | null> =
-  async (
-    _req,
-    _ctx,
-  ) => {
-    // search prodcuts on Oracle. Feel free to change any of these parameters
-    const { data } = await occ.search.productBySlug(
-      "t-shirt-mc-colors-laranja-surfing-67504-08226",
-    );
+const productPageLoader: LoaderFunction<
+  null,
+  ProductDetailsPage | null
+> = async (_req, _ctx) => {
+  // search prodcuts on Oracle. Feel free to change any of these parameters
 
-    const product = data.page.product;
+  const skuId = _ctx.params.slug || DEFAULT_SKU;
 
-    // Product not found, return the 404 status code
-    if (!data) {
-      return {
-        data: null,
-        status: 404,
-      };
-    }
+  const { data } = await occ.search.productBySlug(skuId);
+  const { id } = data.page.product;
 
-    // Convert the OCC product to schema.org format and return it
-    // deno-lint-ignore no-explicit-any
-    return { data: toProductPage(product, undefined as any) };
-  };
+  const { productSkuInventoryStatus } = await occ.search.stockStatus(id);
+
+  const product = data.page.product;
+
+  // Product not found, return the 404 status code
+  if (!data) {
+    return {
+      data: null,
+      status: 404,
+    };
+  }
+
+  // Convert the OCC product to schema.org format and return it
+  return { data: toProductPage(product, productSkuInventoryStatus) };
+};
 
 export default productPageLoader;
