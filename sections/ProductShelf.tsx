09643a9a47e4ad28f43f28d74b33be019ad641ca
gimenes <tlgimenes@gmail.com>
2023-02-15 13:52:37 -0300

    apply requested changes

--- sections/ProductShelf.tsx
+++ sections/ProductShelf.tsx
@@ -13,11 +13,22 @@ export default function ProductShelf({
   products,
 }: Props) {
   return (
-    <div class="max-w-[1400px] w-full p-2 md:p-0 mx-auto">
-      {title && <h2 class="text-center mb-8 text-lg md:text-2xl">{title}</h2>}
-      <ul class="flex flex-nowrap overflow-auto w-full justify-between">
+    <div class="max-w-[1400px] w-full p-2 flex flex-col items-center">
+      {title && <h2 class="mb-8 text-lg md:text-2xl">{title}</h2>}
+      <ul
+        class="flex flex-nowrap overflow-x-auto max-w-full gap-2"
+        // TODO: change this to class-based styling once ported to tailwind 3.0
+        style={{
+          "scroll-snap-type": "x mandatory",
+          "scroll-behavior": "smooth",
+          "-webkit-overflow-scrolling": "touch",
+        }}
+      >
         {products?.map((product, index) => (
-          <li class="min-w-[220px] max-w-[250px] sm:min-w-[260px] sm:max-w-[280px]">
+          <li
+            class="min-w-[220px] max-w-[250px] sm:min-w-[250px] sm:max-w-[280px]"
+            style={{ "scroll-snap-align": "center" }}
+          >
             <ProductCard key={index} {...product} />
           </li>
         ))}
