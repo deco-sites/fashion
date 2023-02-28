23676719feeb35e0a4aefe0d1639101206c38ce0
Igor Brasileiro <brasileiro456@gmail.com>
2023-02-28 12:01:04 -0300

    feat: carousel css only (#64)

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -1,6 +1,5 @@
-import { useId } from "preact/hooks";
+import Slider from "$store/components/ui/Slider.tsx";
 import Text from "$store/components/ui/Text.tsx";
-import Slider from "$store/islands/Slick.tsx";
 
 const messages = [
   "Parcelamento em até 10x no cartão",
@@ -14,37 +13,20 @@ export interface Props {
 }
 
 function Alert({ alerts = messages }: Props) {
-  const id = useId();
-
   return (
-    <>
-      <section
-        id={id}
-        class="flex flex-row w-full justify-between h-10 bg-decorative-two"
-      >
-        <div class="flex gap-2 items-center text-sm text-white m-auto">
-          <div class="w-[340px] sm:w-[500px] overflow-x-hidden">
-            <div
-              data-slider-content
-              class={`w-[${
-                alerts.length * 100
-              }%] transition flex justify-center`}
-            >
-              {alerts.map((alert) => (
-                <Text
-                  class="w-[340px] sm:w-[500px] text-center"
-                  variant="caption-regular"
-                  tone="interactive-default"
-                >
-                  {alert}
-                </Text>
-              ))}
-            </div>
-          </div>
-        </div>
-      </section>
-      <Slider id={id} items={alerts.length} delay={4000} />
-    </>
+    <div class="hidden md:(flex justify-center) bg-decorative-two text-sm text-white">
+      <Slider class="pt-10 w-[340px] sm:w-[500px]" animationDuration={4}>
+        {alerts.map((alert) => (
+          <Text
+            class="flex justify-center items-center w-[340px] sm:w-[500px] h-full"
+            variant="caption-regular"
+            tone="interactive-default"
+          >
+            {alert}
+          </Text>
+        ))}
+      </Slider>
+    </div>
   );
 }
 
--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -1,8 +1,7 @@
-import { useId } from "preact/hooks";
 import { Image as LiveImage } from "$live/std/ui/types/Image.ts";
 import { Picture, Source } from "$live/std/ui/components/Picture.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
-import Slider from "$store/islands/Slick.tsx";
 
 export interface Image {
   /** @description desktop otimized image */
@@ -15,6 +14,14 @@ export interface Image {
   alt: string;
 }
 
+interface Dimension {
+  width: number;
+  height: number;
+}
+
+const aspectRatio = ({ width, height }: Dimension) =>
+  ((height / width) * 100).toFixed(2);
+
 export interface Props {
   images?: Image[];
   /**
@@ -23,89 +30,72 @@ export interface Props {
   preload?: boolean;
 }
 
-function Carousel({ images = [], preload = false }: Props) {
-  const id = useId();
+function Carousel({ images, preload }: Props) {
+  const mobileDimension: Dimension = {
+    width: 360,
+    height: 331,
+  };
+
+  const desktopDimension: Dimension = {
+    width: 1366,
+    height: 517,
+  };
 
   return (
-    <div
-      id={id}
-      class="relative w-full overflow-hidden"
+    <Slider
+      // this padding top (pt) is the aspect-ratio (height/width) value from the image below for each viewport
+      class={`w-full pt-[${aspectRatio(mobileDimension)}%] sm:pt-[${
+        aspectRatio(desktopDimension)
+      }%]`}
+      animationDuration={5}
+      leftArrow={
+        <Icon
+          width={24}
+          height={24}
+          id="ChevronLeft"
+          strokeWidth={3}
+        />
+      }
+      rightArrow={
+        <Icon
+          width={24}
+          height={24}
+          id="ChevronRight"
+          strokeWidth={3}
+        />
+      }
+      dot={<Icon id="Circle" width={24} height={24} strokeWidth={2} />}
     >
-      <ul
-        data-slider-content
-        class="flex flex-nowrap transition"
-        style={{ width: `calc(${images.length}*100vw)` }}
-      >
-        {images.map(({ desktop, mobile, href, alt }, index) => {
-          const lcp = index === 0 && preload;
+      {images?.map(({ mobile, desktop, alt }, index) => {
+        const isFirst = index === 0;
+        const lcp = isFirst && preload;
 
-          return (
-            <li>
-              <a href={href}>
-                <Picture class="w-screen block" preload={lcp}>
-                  <Source
-                    media="(max-width: 767px)"
-                    fetchPriority={lcp ? "high" : "auto"}
-                    src={mobile}
-                    width={360}
-                    height={600}
-                  />
-                  <Source
-                    media="(min-width: 768px)"
-                    fetchPriority={lcp ? "high" : "auto"}
-                    src={desktop}
-                    width={1366}
-                    height={600}
-                  />
-                  <img
-                    class="object-cover w-full"
-                    loading={lcp ? "eager" : "lazy"}
-                    src={desktop}
-                    alt={alt}
-                  />
-                </Picture>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-
-      {images.length > 0 && (
-        <>
-          {/* Next/Prev button Controls */}
-          <button
-            aria-label="previous banner image"
-            class="absolute top-1/2 left-0 ml-2 text-white outline-none p-2"
-            data-slider-prev
-          >
-            <Icon width={24} height={24} id="ChevronLeft" strokeWidth={3} />
-          </button>
-          <button
-            aria-label="next banner image"
-            class="absolute top-1/2 right-0 mr-2 text-white outline-none p-2"
-            data-slider-next
-          >
-            <Icon width={24} height={24} id="ChevronRight" strokeWidth={3} />
-          </button>
-
-          {/* Dots buttons, usually bellow main image */}
-          <div class="absolute w-full top-[80%] flex justify-center flex-nowrap">
-            {images.map((_, id) => (
-              <button
-                aria-label={`Display ${id} banner`}
-                class="p-2 rounded-full text-white disabled:text-gray-600 outline-none"
-                data-dot
-              >
-                <Icon id="Circle" width={24} height={24} strokeWidth={2} />
-              </button>
-            ))}
-          </div>
-
-          {/* Effects for transitioning between images */}
-          <Slider items={images.length} id={id} delay={5_000} />
-        </>
-      )}
-    </div>
+        return (
+          <Picture class="w-screen block" preload={lcp}>
+            <Source
+              media="(max-width: 767px)"
+              fetchPriority={lcp ? "high" : "auto"}
+              src={mobile}
+              width={360}
+              height={331}
+            />
+            <Source
+              media="(min-width: 768px)"
+              fetchPriority={lcp ? "high" : "auto"}
+              src={desktop}
+              width={1366}
+              height={517}
+            />
+            <img
+              class="object-cover w-full"
+              loading={lcp ? "eager" : "lazy"}
+              src={desktop}
+              alt={alt}
+            />
+          </Picture>
+        );
+      })}
+    </Slider>
   );
 }
 
