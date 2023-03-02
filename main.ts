3596245fbba19ede1f1c6e92c1e6161990165d9f
gimenes <tlgimenes@gmail.com>
2023-03-02 09:15:09 -0300

    remove prefetch

--- main.ts
+++ main.ts
@@ -7,13 +7,13 @@ import manifest from "./fresh.gen.ts";
 import { start } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
-import prefetchPlugin from "prefetch";
 import partytownPlugin from "partytown/mod.ts";
+// import prefetchPlugin from "prefetch";
 
 await start(manifest, {
   plugins: [
     partytownPlugin(),
-    prefetchPlugin(),
+    // prefetchPlugin(),
     twindPlugin({
       ...twindConfig,
       selfURL: new URL("./twind.config.ts", import.meta.url).href,
