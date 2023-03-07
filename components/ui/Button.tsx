f43d8414acf0ecc24b81e3df349c1d9e82c6844b
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2023-03-07 14:34:10 -0300

    Change icon button hover

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -20,7 +20,7 @@ const variants = {
   tertiary:
     "h-[36px] px-3 rounded bg-interactive-inverse font-button text-button border-transparent hover:bg-hover active:border-interactive disabled:border-transparent disabled:text-subdued focus:outline-none",
   icon:
-    "h-[36px] px-2 bg-transparent rounded-full text-default border-transparent active:bg-interactive active:text-default-inverse disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
+    "h-[36px] px-2 rounded-full bg-transparent text-default border-transparent hover:bg-hover active:bg-interactive active:text-default-inverse disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(({
