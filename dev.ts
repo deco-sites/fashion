d4f0173a7098f61bb1bf75a5501631895917150a
Marcos Candeia <marrcooos@gmail.com>
2023-03-17 12:39:47 -0300

    Use live gh version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev.ts
+++ dev.ts
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch=static/
 import dev from "$live/dev.ts";
-import liveManifest from "$live/fresh.gen.ts";
-import liveStdManifest from "deco-sites/std/fresh.gen.ts";
+import liveManifest from "$live/live.gen.ts";
+import liveStdManifest from "deco-sites/std/live.gen.ts";
 
 await dev(import.meta.url, "./main.ts", {
-  imports: {
-    "$live": liveManifest,
-    "deco-sites/std": liveStdManifest,
-  },
+  imports: [liveManifest, liveStdManifest],
 });
