a3319399b2a92b97887fff31840aa952383ccefa
gimenes <tlgimenes@gmail.com>
2023-03-24 08:57:44 -0300

    add linktree

---
+++ sections/LinkTree.tsx
@@ -0,0 +1,3 @@
+export { default } from "$store/components/ui/LinkTree.tsx";
+export type { Props, Link, Social } from "$store/components/ui/LinkTree.tsx"
+export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
