937f89cb30ca71aa23c6b9cca923069fef5e672c
gimenes <tlgimenes@gmail.com>
2023-04-02 10:39:23 -0300

    lint

--- sections/BannerGrid.tsx
+++ sections/BannerGrid.tsx
@@ -1,3 +1,3 @@
 export { default } from "$store/components/ui/BannerGrid.tsx";
-export type { Props, Banner } from "$store/components/ui/BannerGrid.tsx";
+export type { Banner, Props } from "$store/components/ui/BannerGrid.tsx";
 export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
