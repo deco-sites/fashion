8a6e8d593a6667180d7056b1aa680b391d566801
Marcos Candeia <marrcooos@gmail.com>
2023-04-03 18:40:16 -0300

    Migrating from live v0 (#123)
    * Migrating from live v0
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use live compatibility version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update import_map.json
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use new std ver
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add deno lock
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Fix global section previews
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add flags support
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Do not reuse flag from cookie if no cache
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Force change
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Improve schema gen
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add support to state
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add integrity check
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Fix schemas title
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Remove workbench support and include configstore page support
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Fix schema gen
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use rc.2
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add rc.5
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add chatgpt code review
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add chatgpt code review
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Bump live to 1.0.0-rc.7 - JSONViewer
    
    * Autoformat
    
    * Update to rc.8
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use rc.9
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use rc.10
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add major info live rc.11
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use live rc12
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use latest live rc
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live rc version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update to rc.18
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update live version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update rc version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * update live rc
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update to rc.28
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    Co-authored-by: Lucis <luciannojunior@gmail.com>

--- dev.ts
+++ dev.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env -S deno run -A --watch=static/
 import dev from "$live/dev.ts";
-import liveManifest from "$live/fresh.gen.ts";
-import liveStdManifest from "deco-sites/std/fresh.gen.ts";
+import liveManifest from "$live/live.gen.ts";
+import liveStdManifest from "deco-sites/std/live.gen.ts";
 
 await dev(import.meta.url, "./main.ts", {
+  siteId: 239,
   imports: {
     "$live": liveManifest,
     "deco-sites/std": liveStdManifest,
