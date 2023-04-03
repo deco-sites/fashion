dc2e3780c87f18beaa718134c95682285f491576
Tiago Gimenes <tlgimenes@gmail.com>
2023-04-03 11:29:16 -0300

    feat: Add banner PLP section (#136)

---
+++ components/ui/BannerPLP.tsx
@@ -0,0 +1,92 @@
+import Text from "$store/components/ui/Text.tsx";
+import Container from "$store/components/ui/Container.tsx";
+import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
+import type { LoaderReturnType } from "$live/types.ts";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import type { ProductListingPage } from "deco-sites/std/commerce/types.ts";
+
+export interface Banner {
+  /** @description RegExp to enable this banner on the current URL. Use /feminino/* to display this banner on feminino category  */
+  matcher: string;
+  /** @description text to be rendered on top of the image */
+  title?: string;
+  /** @description text to be rendered on top of the image */
+  subtitle?: string;
+  image: {
+    /** @description Image for big screens */
+    desktop: LiveImage;
+    /** @description Image for small screens */
+    mobile: LiveImage;
+    /** @description image alt text */
+    alt?: string;
+  };
+}
+
+export interface Props {
+  page?: LoaderReturnType<ProductListingPage | null>;
+  banners?: Banner[];
+}
+
+function BannerUI({ banner }: { banner: Banner }) {
+  const { title, subtitle, image } = banner;
+
+  return (
+    <div class="grid grid-cols-1 grid-rows-1">
+      <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
+        <Source
+          src={image.mobile}
+          width={360}
+          height={120}
+          media="(max-width: 767px)"
+        />
+        <Source
+          src={image.desktop}
+          width={1440}
+          height={200}
+          media="(min-width: 767px)"
+        />
+        <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
+      </Picture>
+
+      <Container class="flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
+        <h1>
+          <Text variant="heading-1" tone="default-inverse">
+            {title}
+          </Text>
+        </h1>
+        <h2>
+          <Text variant="heading-3" tone="default-inverse">
+            {subtitle}
+          </Text>
+        </h2>
+      </Container>
+    </div>
+  );
+}
+
+/**
+ * TODO: run the matcher agains the true URL instead on the breadcrumb.
+ * This way we can remove the need for a loader. This can be done on live@1.x
+ */
+function Banner({ page, banners = [] }: Props) {
+  if (!page || page.breadcrumb.itemListElement.length === 0) {
+    return null;
+  }
+
+  const { item: canonical } = page
+    .breadcrumb
+    .itemListElement
+    .reduce((curr, acc) => curr.position > acc.position ? curr : acc);
+
+  const matching = banners.find(({ matcher }) =>
+    new RegExp(matcher).test(canonical)
+  );
+
+  if (!matching) {
+    return null;
+  }
+
+  return <BannerUI banner={matching} />;
+}
+
+export default Banner;
