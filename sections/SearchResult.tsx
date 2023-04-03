149d31d9088397fa0b5c652a746d15f5cb7e84af
Tiago Gimenes <tlgimenes@gmail.com>
2023-04-03 11:22:34 -0300

    feat: Aside filters (#139)

---
+++ sections/SearchResult.tsx
@@ -0,0 +1,7 @@
+import SearchResult, { Props } from "$store/components/search/SearchResult.tsx";
+
+function SearchResultSection(props: Props) {
+  return <SearchResult {...props} />;
+}
+
+export default SearchResultSection;
