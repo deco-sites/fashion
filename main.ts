3b6738a371c3816c453e688de2333ac82120b3e2
Tiago Gimenes <tlgimenes@gmail.com>
2023-04-05 12:33:03 -0300

    remove prefetch (#147)

--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@ import twindPlugin from "$fresh/plugins/twind.ts";
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
-import prefetchPlugin from "prefetch";
 import manifest from "./live.gen.ts";
 import twindConfig from "./twind.config.ts";
 import site from "./site.json" assert { type: "json" };
@@ -15,7 +14,6 @@ import site from "./site.json" assert { type: "json" };
 await start($live(manifest, site), {
   plugins: [
     partytownPlugin(),
-    prefetchPlugin(),
     twindPlugin({
       ...twindConfig,
       selfURL: new URL("./twind.config.ts", import.meta.url).href,
