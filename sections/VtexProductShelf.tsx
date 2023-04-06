cb9d2a5cef2ca13a4060aaaff656f72258dbde48
Marcos Candeia <marrcooos@gmail.com>
2023-04-06 14:51:32 -0300

    Shopify or VTEX loader selector
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- sections/VtexProductShelf.tsx
+++ sections/VtexProductShelf.tsx
@@ -3,24 +3,34 @@ import ProductShelf, {
   Props as ProductShelfProps,
 } from "deco-sites/fashion/components/product/ProductShelf.tsx";
 import { StateVTEX } from "deco-sites/std/commerce/vtex/types.ts";
-import loader, {
-  Props as LoaderProps,
+import {
+  default as shopifyLoader,
+  default as vtexLoader,
+  Props as ShopifyLoaderProps,
+  Props as VtexLoaderProps,
 } from "deco-sites/std/functions/vtexProductList.ts";
 import { LiveConfig } from "https://denopkg.com/deco-cx/live@1.0.0-rc.36/blocks/handler.ts";
 
 function ProductShelfSection(props: ProductShelfProps) {
   return <ProductShelf {...props} />;
 }
-
+export interface OtherProps {
+  props: string;
+}
 export async function getProps(
   request: Request,
   ctx: HandlerContext<
     unknown,
-    LiveConfig<LoaderProps, StateVTEX>
+    LiveConfig<
+      | VtexLoaderProps & { type: "VTEX" }
+      | ShopifyLoaderProps & { type: "Shopify" },
+      StateVTEX
+    >
   >,
-): Promise<Pick<ProductShelfProps, "products" | "title">> {
+): Promise<Pick<ProductShelfProps, "products">> {
+  const loader = (ctx.state.$live.type === "VTEX") ? vtexLoader : shopifyLoader;
   const { data: products } = await loader(request, ctx, ctx.state.$live);
-  return { products, title: "Marcos Candeia" };
+  return { products };
 }
 
 export default ProductShelfSection;
