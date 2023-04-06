4dc0bfd2e0efbeff7f77a392e5fa8db1877d10d9
Marcos Candeia <marrcooos@gmail.com>
2023-04-05 22:49:34 -0300

    Generate only the necessary data
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- sections/VtexProductShelf.tsx
+++ sections/VtexProductShelf.tsx
@@ -2,17 +2,11 @@ import { HandlerContext } from "$fresh/src/server/types.ts";
 import ProductShelf, {
   Props as ProductShelfProps,
 } from "deco-sites/fashion/components/product/ProductShelf.tsx";
-import { Product } from "deco-sites/std/commerce/types.ts";
 import { StateVTEX } from "deco-sites/std/commerce/vtex/types.ts";
 import loader, {
   Props as LoaderProps,
 } from "deco-sites/std/functions/vtexProductList.ts";
 import { LiveConfig } from "https://denopkg.com/deco-cx/live@1.0.0-rc.36/blocks/handler.ts";
-
-export interface Props {
-  title: string;
-  itemsPerPage?: number;
-}
 
 function ProductShelfSection(props: ProductShelfProps) {
   return <ProductShelf {...props} />;
@@ -22,11 +16,11 @@ export async function getProps(
   request: Request,
   ctx: HandlerContext<
     unknown,
-    LiveConfig<Props & LoaderProps, StateVTEX>
+    LiveConfig<LoaderProps, StateVTEX>
   >,
-): Promise<ProductShelfProps> {
+): Promise<Pick<ProductShelfProps, "products">> {
   const { data: products } = await loader(request, ctx, ctx.state.$live);
-  return { ...ctx.state.$live, products };
+  return { products };
 }
 
 export default ProductShelfSection;
