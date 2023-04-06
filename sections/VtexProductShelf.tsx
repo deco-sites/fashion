509c0b15437365309c91b571f9b1cea76bfe8ced
Marcos Candeia <marrcooos@gmail.com>
2023-04-05 21:32:11 -0300

    Update live version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- sections/VtexProductShelf.tsx
+++ sections/VtexProductShelf.tsx
@@ -1,12 +1,13 @@
+import { HandlerContext } from "$fresh/src/server/types.ts";
 import ProductShelf, {
   Props as ProductShelfProps,
 } from "deco-sites/fashion/components/product/ProductShelf.tsx";
+import { Product } from "deco-sites/std/commerce/types.ts";
+import { StateVTEX } from "deco-sites/std/commerce/vtex/types.ts";
 import loader, {
   Props as LoaderProps,
 } from "deco-sites/std/functions/vtexProductList.ts";
-import { HandlerContext } from "https://deno.land/x/fresh@1.1.4/src/server/types.ts";
 import { LiveConfig } from "https://denopkg.com/deco-cx/live@1.0.0-rc.36/blocks/handler.ts";
-import { StateVTEX } from "https://denopkg.com/deco-sites/std@1.0.0-rc.9/commerce/vtex/types.ts";
 
 export interface Props {
   title: string;
