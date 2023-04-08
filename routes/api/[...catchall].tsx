535a21a2e4a2544158e19b2954796e8473b7c8f4
gimenes <tlgimenes@gmail.com>
2023-04-08 11:51:14 -0300

    improve wishlist

--- routes/api/[...catchall].tsx
+++ routes/api/[...catchall].tsx
@@ -3,7 +3,7 @@ import { getSetCookies, setCookie } from "std/http/mod.ts";
 
 const proxyTo = `https://bravtexfashionstore.vtexcommercestable.com.br/api`;
 
-const hopByHop = [
+const HOP_BY_HOP = [
   "Keep-Alive",
   "Transfer-Encoding",
   "TE",
@@ -12,35 +12,36 @@ const hopByHop = [
   "Upgrade",
   "Proxy-Authorization",
   "Proxy-Authenticate",
-
-  // Somehow some APIs at VTEX break when using this
-  "origin",
 ];
 
 const proxy: Handlers["GET"] = async (req, ctx) => {
   const url = new URL(req.url);
-  const headers = new Headers(req.headers);
-
-  hopByHop.forEach((h) => headers.delete(h));
-
   const qs = url.searchParams.toString();
+  const to = new URL(`${proxyTo}/${ctx.params.catchall}?${qs}`);
 
-  const response = await fetch(
-    `${proxyTo}/${ctx.params.catchall}?${qs}`,
-    {
-      headers,
-      redirect: "manual",
-      method: req.method,
-      body: req.body,
-    },
-  );
+  const headers = new Headers(req.headers);
+  HOP_BY_HOP.forEach((h) => headers.delete(h));
+  headers.set("origin", to.origin);
+  headers.set("host", to.host);
+  headers.set("x-forwarded-host", url.host);
+
+  const response = await fetch(to, {
+    headers,
+    redirect: "manual",
+    method: req.method,
+    body: req.body,
+  });
 
   // Change cookies domain
   const responseHeaders = new Headers(response.headers);
   const cookies = getSetCookies(responseHeaders);
   responseHeaders.delete("set-cookie");
-  for (const cookie of cookies) {
-    setCookie(responseHeaders, { ...cookie, domain: url.hostname });
+
+  // Setting cookies on GET requests prevent cache from cdns, slowing down the app
+  if (req.method !== "GET") {
+    for (const cookie of cookies) {
+      setCookie(responseHeaders, { ...cookie, domain: url.hostname });
+    }
   }
 
   return new Response(response.body, {
