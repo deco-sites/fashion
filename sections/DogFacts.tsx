ac3ac72197f5edcce3b48e432d5a69e1ac3af9a3
Marcos Candeia <marrcooos@gmail.com>
2023-04-08 14:47:10 -0300

    Add dog facts
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- sections/DogFacts.tsx
+++ sections/DogFacts.tsx
@@ -1,33 +1,31 @@
-import { PropsLoader } from "$live/mod.ts";
-import dogApiFacts, {
-  DogFact,
-  Props as DogFactsProps,
-} from "deco-sites/fashion/loaders/dogApiFacts.ts";
+import type { LoaderContext } from "$live/types.ts";
+import type { SectionProps } from "$live/mod.ts";
 
-export interface Props {
-  title: string;
-  dogFacts: DogFact[];
-}
-
-export default function DogFacts({ title, dogFacts }: Props) {
+export default function DogFacts(
+  { title, dogFacts }: SectionProps<typeof loader>,
+) {
   return (
     <div class="p-4">
       <h1 class="font-bold">{title}</h1>
       <ul>
-        {dogFacts.map(({ fact }) => <li>{fact}</li>)}
+        {dogFacts.map((fact) => <li>{fact}</li>)}
       </ul>
     </div>
   );
 }
 
 // Props type that will be configured in deco.cx's Admin
-export interface LoadProps extends DogFactsProps {
+export interface LoaderProps {
   title: string;
+  numberOfFacts?: number;
 }
 
-export const loader: PropsLoader<
-  Props,
-  LoadProps
-> = {
-  dogFacts: dogApiFacts,
-};
+export async function loader(
+  _req: Request,
+  { state: { $live: { numberOfFacts, title } } }: LoaderContext<LoaderProps>,
+) {
+  const { facts: dogFacts } = (await fetch(
+    `https://dogapi.dog/api/facts?number=${numberOfFacts ?? 1}`,
+  ).then((r) => r.json())) as { facts: string[] };
+  return { dogFacts, title };
+}
