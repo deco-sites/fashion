b5e12eac3157285bb49cd5b7b26ea064c783eab6
Marcos Candeia <marrcooos@gmail.com>
2023-04-15 08:59:24 -0300

    Add extension changes
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- extensions/changeProductCategory.ts
+++ extensions/changeProductCategory.ts
@@ -1,23 +1,14 @@
 import { Product } from "deco-sites/std/commerce/types.ts";
+import { ExtensionOf } from "../../live/blocks/extension.ts";
 
 export interface Props {
   newCategory: string;
 }
 
-const changeCategory =
-  (category: string) => (_p: Product): Partial<Product> => ({
-    category,
-  });
 export default function addProductPrice(
-  product: Product | Product[] | null,
   { newCategory }: Props,
-): Partial<Product> | Partial<Product>[] | null {
-  if (product === null) {
-    return product;
-  }
-  const withCategory = changeCategory(newCategory);
-  if (Array.isArray(product)) {
-    return product.map(withCategory);
-  }
-  return withCategory(product);
+): ExtensionOf<Product> {
+  return {
+    category: (_, cat) => (cat ?? "nao tem") + newCategory,
+  };
 }
--- extensions/changeProductPrice.ts
+++ extensions/changeProductPrice.ts
@@ -1,22 +1,20 @@
 import { Product } from "deco-sites/std/commerce/types.ts";
+import { ExtensionOf } from "../../live/blocks/extension.ts";
 
 export interface Props {
   newPrice: number;
 }
 
-const addPrice = (price: number) => (p: Product): Partial<Product> => ({
-  offers: p?.offers ? { ...p.offers, lowPrice: price } : undefined,
-});
 export default function addProductPrice(
-  product: Product | Product[] | null,
   { newPrice }: Props,
-): Partial<Product> | Partial<Product>[] | null {
-  if (product === null) {
-    return product;
-  }
-  const withPrice = addPrice(newPrice);
-  if (Array.isArray(product)) {
-    return product.map(withPrice);
-  }
-  return withPrice(product);
+): ExtensionOf<Product> {
+  return {
+    offers: {
+      offers: {
+        _forEach: {
+          price: (_, current) => current + newPrice,
+        },
+      },
+    },
+  };
 }
