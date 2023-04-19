27a46d3a836fd41d3123aca59e1e7da10a0c83cf
gimenes <tlgimenes@gmail.com>
2023-04-19 10:18:27 -0300

    improve tailwindcss generation

--- dev.ts
+++ dev.ts
@@ -1,18 +1,12 @@
-#!/usr/bin/env -S deno run -A --watch=static/
+#!/usr/bin/env -S deno run -A --watch
 import dev from "$live/dev.ts";
 import liveManifest from "$live/live.gen.ts";
 import liveStdManifest from "deco-sites/std/live.gen.ts";
 
 // Generate tailwind css rules
-const start = performance.now()
 await Deno.run({
   cmd: [Deno.execPath(), "task", "tailwindcss"],
-  stdin: "piped",
-  stdout: "piped",
-  stderr: "piped",
 }).status();
-const duration = performance.now() - start
-console.info(`Generating Tailwind CSS stylesheet took ${duration.toFixed(0)}ms`)
 
 await dev(import.meta.url, "./main.ts", {
   imports: {
