c1af80c67d8b1a46240709120612c4367cd64ce2
Marcos Candeia <marrcooos@gmail.com>
2023-04-30 18:30:00 -0300

    Use live workflows version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ workflows/simple.ts
@@ -0,0 +1,26 @@
+import { Workflow, WorkflowContext, WorkflowGen } from "$live/mod.ts";
+import type { Manifest } from "../live.gen.ts";
+export interface Props {
+  sleepTime: number;
+}
+export default function Simple({ sleepTime }: Props): Workflow {
+  return function* (_ctx: WorkflowContext<Manifest>): WorkflowGen<void> {
+    const chargedValue1 = yield _ctx.invoke({
+      key: "deco-sites/fashion/actions/charge.ts",
+      props: {
+        value: 20,
+      },
+    });
+    console.log("the charged value", chargedValue1);
+    console.log("sleeping", sleepTime);
+    yield _ctx.sleep(sleepTime);
+    const chargedValue2 = yield _ctx.invoke({
+      key: "deco-sites/fashion/actions/charge.ts",
+      props: {
+        value: 20,
+      },
+    });
+    console.log("the charged value", chargedValue2);
+    console.log("TOTAL", chargedValue1.value + chargedValue2.value);
+  };
+}
