a70cb689ea2914a2d5fd74ad46e456b49146db3b
gimenes <tlgimenes@gmail.com>
2023-05-19 16:17:36 -0300

    alert

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -16,7 +16,7 @@ function Alert({ alerts = [], interval = 5 }: Props) {
 
   return (
     <div id={id}>
-      <Slider class="carousel carousel-center bg-secondary gap-6 scrollbar-none">
+      <Slider class="carousel carousel-center bg-primary gap-6 scrollbar-none">
         {alerts.map((alert, index) => (
           <Slider.Item index={index} class="carousel-item">
             <span class="text-sm text-secondary-content flex justify-center items-center w-screen h-[38px]">
