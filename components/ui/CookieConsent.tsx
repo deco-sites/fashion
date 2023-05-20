7047715bdb78218b0658ade9948a5a909a8e2291
Vitor Veloso <vitor@Vitors-MacBook-Pro.local>
2023-05-20 15:12:31 -0300

    :lipstick: Updating CookieConsent.tsx

--- components/ui/CookieConsent.tsx
+++ components/ui/CookieConsent.tsx
@@ -32,12 +32,15 @@ function CookieConsent() {
     <>
       <div
         id={id}
-        class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 w-screen z-50"
+        class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 z-50"
       >
-        <div class="container px-4 py-4 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
+        <div class="m-4 px-4 py-4 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
+          <div className='flex justify-between w-full'>
+            <span className='text-xl'>Cookie Policy</span>
+            <img src='/static/svg/close.svg' alt="Close button" />
+          </div>
           <span class="flex-grow text-sm">
-            Utilizamos cookies em nosso site para melhorar seu desempenho,
-            segurança e personalizar conteúdo e anúncios.
+            We use third-party cookies order to personalize your experience.
           </span>
 
           <a href="https://www.deco.cx">
---
+++ sections/TestComp.tsx
@@ -0,0 +1,16 @@
+export type teste = {
+  a: string;
+  b?: string;
+}
+
+export default function TestComp({a, b}: teste) {
+  return (
+    <>
+      <div>TestComp</div>
+      <div>
+        <h2>{a}</h2>
+        <h3>{b}</h3>
+      </div>
+    </>
+  )
+}
