30af369916fd78290cae9bcbb6c6f1028d2ff463
raissa campos <raissa.campos26@gmail.com>
2023-05-20 18:55:04 +0100

    feat: desktop

--- components/ui/ImageGallery.tsx
+++ components/ui/ImageGallery.tsx
@@ -34,11 +34,13 @@ export default function ImageGallery({
 }: Props) {
   return (
     <section class="px-4 max-w-[1360px] lg:m-auto">
-      <h1 class="text-base-content text-4xl text-center mt-10 mb-4 lg:text-6xl lg:mb-6">{title}</h1>
+      <h1 class="text-base-content text-4xl text-center mt-10 mb-4 lg:text-6xl lg:mb-6">
+        {title}
+      </h1>
       <p class="text-center text-base-content text-2xl leading-7">
         {description}
       </p>
-      <div class="grid grid-cols-2 gap-4 mt-11 lg:flex lg:gap-8 lg:mt-20" >
+      <div class="grid grid-cols-2 gap-4 mt-11 lg:flex lg:gap-8 lg:mt-20">
         <img class="w-full object-cover" src={imageOne} alt={alt} />
         <img class="w-full object-cover" src={imageTwo} alt={alt} />
         <img class="w-full object-cover" src={imageThree} alt={alt} />
