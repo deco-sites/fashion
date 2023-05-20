ed52bba24018f021048acec6ec90512bba2e6aa8
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-20 16:25:49 -0300

    feat criando estrutura inicial do timer

---
+++ sections/Timers/CampaignTimer.Edubias.tsx
@@ -0,0 +1,33 @@
+export interface Props {
+  title: string,
+  hours?: number,
+  minutes?: number,
+  seconds: number,
+}
+
+export default function CampaignTimer({ title, hours = 0, minutes = 0, seconds = 0 }: Props) {
+  return (
+    <div className="h-28 p-4 bg-red-400 flex flex-col justify-center items-center text-white">
+      <h2 className="text-sm mb-4">{title}</h2>
+      <div className="flex">
+        <div className="flex">
+          <div>
+            <p>{hours}</p>
+            <p>Hours</p>
+          </div>
+          <p>:</p>
+          <div>
+            <p>{minutes}</p>
+            <p>Minutes</p>
+          </div>
+          <p>:</p>
+          <div>
+            <p>{seconds}</p>
+            <p>Seconds</p>
+          </div>
+        </div>
+        <button>Button</button>
+      </div>
+    </div>
+  )
+}
