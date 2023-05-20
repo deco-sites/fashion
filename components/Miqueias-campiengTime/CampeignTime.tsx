a9f607263626dd9ee50cdfde6fdc3894336d7f62
CaioRig <caiorigotto@gmail.com>
2023-05-20 18:49:19 -0300

    ...

--- components/Miqueias-campiengTime/CampeignTime.tsx
+++ components/Miqueias-campiengTime/CampeignTime.tsx
@@ -1,5 +1,5 @@
 import CountdownCampingTime from "../../islands/CountdownCampingTime.tsx";
-import TextCampeignTime from './TextCampeignTime.tsx'
+import TextCampeignTime from "./TextCampeignTime.tsx";
 
 export interface Props {
   hours: number;
@@ -12,27 +12,43 @@ export interface Props {
 }
 
 export default function CampignTime(
-  { hours, minutes, second, campaignDescription, clickableLinkDescription, campaignLink, buttonDecription }: Props,
+  {
+    hours,
+    minutes,
+    second,
+    campaignDescription,
+    clickableLinkDescription,
+    campaignLink,
+    buttonDecription,
+  }: Props,
 ) {
   return (
     <div class="w-full h-19 bg-red-300 flex items-center justify-center gap-4">
       <div class="container py-2 flex items-center justify-around flex-col mb-4 sm:flex-row">
-          <div class="block sm:hidden">
-            <TextCampeignTime textInput={campaignDescription} nameLink={clickableLinkDescription} link={campaignLink}/>
-          </div>
-          <div class="w-full flex flex-row gap-6 items-center justify-center sm:gap-16">
-            <CountdownCampingTime
+        <div class="block sm:hidden">
+          <TextCampeignTime
+            textInput={campaignDescription}
+            nameLink={clickableLinkDescription}
+            link={campaignLink}
+          />
+        </div>
+        <div class="w-full flex flex-row gap-6 items-center justify-center sm:gap-16">
+          <CountdownCampingTime
             HoursInput={hours}
             MinutesInput={minutes}
             SecondInput={second}
           />
           <div class="hidden sm:block">
-            <TextCampeignTime textInput={campaignDescription} nameLink={clickableLinkDescription} link={campaignLink}/>
+            <TextCampeignTime
+              textInput={campaignDescription}
+              nameLink={clickableLinkDescription}
+              link={campaignLink}
+            />
           </div>
           <button class="px-4 py-2 bg-black text-white rounded transition hover:bg-slate-700">
-            {buttonDecription ? "Button" : {buttonDecription} }
+            {buttonDecription ? "Button" : { buttonDecription }}
           </button>
-          </div>
+        </div>
       </div>
     </div>
   );
--- components/Miqueias-campiengTime/TextCampeignTime.tsx
+++ components/Miqueias-campiengTime/TextCampeignTime.tsx
@@ -1,26 +1,27 @@
 export interface Props {
-    textInput: string,
-    nameLink: string,
-    link: string
+  textInput: string;
+  nameLink: string;
+  link: string;
 }
 
-export default function TextCampeignTime({textInput, nameLink, link}: Props){
-    return (
-        <>
-          {textInput ? (
-            <p class="text-white font-sans font-normal text-lg">
-              {textInput}{" "}
-              <a href={link} class="underline cursor-pointer">{nameLink}</a>
-            </p>
-          )
-          : (
-            <p class="text-white font-sans font-normal text-lg">
-              Time left for a campaign to end wth a{" "}
-              <a href={!link ? "" : link} class="underline cursor-pointer">
-                link
-              </a>
-            </p>
-          )}
-        </>
-    )
+export default function TextCampeignTime({ textInput, nameLink, link }: Props) {
+  return (
+    <>
+      {textInput
+        ? (
+          <p class="text-white font-sans font-normal text-lg">
+            {textInput}{" "}
+            <a href={link} class="underline cursor-pointer">{nameLink}</a>
+          </p>
+        )
+        : (
+          <p class="text-white font-sans font-normal text-lg">
+            Time left for a campaign to end wth a{" "}
+            <a href={!link ? "" : link} class="underline cursor-pointer">
+              link
+            </a>
+          </p>
+        )}
+    </>
+  );
 }
