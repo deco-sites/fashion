c7ac094ae0302e7778b149448af6aeefea24ddaa
José Augusto Rufino <jose.rufino.ext@synchro.com.br>
2023-05-20 17:59:23 -0300

    Finaliza section cookie consent

--- sections/CookieConsent/CookieConsent.empurra-team.tsx
+++ sections/CookieConsent/CookieConsent.empurra-team.tsx
@@ -2,10 +2,10 @@ import Button from "$store/components/ui/Button.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
 
 export interface Props {
-  policy: string,
-  useCookies: string, 
-  buttonText: string,
-  readCookie: string
+  policy: string;
+  useCookies: string;
+  buttonText: string;
+  readCookie: string;
 }
 
 function CookieConsent({ policy, useCookies, readCookie, buttonText }: Props) {
@@ -17,25 +17,36 @@ function CookieConsent({ policy, useCookies, readCookie, buttonText }: Props) {
           <p class="py-3 ">
             {useCookies}
           </p>
-          <a href="https://www.deco.cx" class="flex items-center text-[#6D8B61] text-sm underline">
+          <a
+            href="https://www.deco.cx"
+            class="flex items-center text-[#6D8B61] text-sm underline"
+          >
             <span>{readCookie}</span>
             <Icon
-                id="ChevronRight"
-                size={14}
-                stroke="currentColor"
-                strokeWidth={2}
-              />
+              id="ChevronRight"
+              size={14}
+              stroke="currentColor"
+              strokeWidth={2}
+            />
           </a>
         </h1>
-          
-          <button
-              class="order-last self-end btn-outline flex items-start btn-outline rounded-none border-none justify-end align-top hover:bg-white w-main p-0 m-0 h-min">
-              <Icon  class=" group-hover:text-black" id="XMark" width={20} height={20} strokeWidth={2} />
-          </button>
 
-          <button class="ml-auto self-end bg-slate-700 rounded-none border-none font-default" style="height: 40px; width: 80px;">
-              <text class="text-white normal-case font-light">{buttonText}</text>
-          </button>
+        <button
+          class="ml-auto self-end bg-slate-700 rounded-none border-none font-default"
+          style="height: 40px; width: 80px;"
+        >
+          <text class="text-white normal-case font-light">{buttonText}</text>
+        </button>
+
+        <button class="fixed top-5 right-12 order-last self-end btn-outline flex items-start btn-outline rounded-none border-none justify-end align-top w-main p-0 m-0 h-min">
+          <Icon
+            class=" group-hover:text-black"
+            id="XMark"
+            width={20}
+            height={20}
+            strokeWidth={2}
+          />
+        </button>
       </div>
     </>
   );
