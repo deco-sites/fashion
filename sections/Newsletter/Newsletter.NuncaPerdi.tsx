f6b43e48cf2b23c4e7753d67585784247765dec6
Rose Gates <pedroluiz2109@gmail.com>
2023-05-20 17:20:02 -0300

    test: newslatter

---
+++ sections/Newsletter/Newsletter.NuncaPerdi.tsx
@@ -0,0 +1,36 @@
+export type TitleItem = {
+    text: string;
+}
+
+export type ContentItem = {
+    input: {type: string, placeholder: string}
+    button: {href: string, text: string}
+    description: {text: string, link: string};
+}
+
+export interface Props {
+    title: TitleItem;
+    content: ContentItem;
+}
+
+function ModalNewsLetter(
+    { title, content }: Props,
+) {
+    return (
+        <div class="hero min-h-screen bg-base-200">
+            <div class="flex flex-col">
+                <div class="hero-content text-center text-6xl">title.text</div>
+
+                <div class="hero-content flex-col text-center">
+                    <div>
+                        <input class="input-bordered input input-ghost border-" type={content.input.type} placeholder={content.input.placeholder} />
+                        <button class="btn btn-outline rounded-none" href={content.button.href}>{content.button.text}</button>
+                    </div>
+                    <p> {content.description.text} <span class="link">{content.description.link}</span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ModalNewsLetter;
