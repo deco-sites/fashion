577a3a2a4f90d71552d99b760a11b41bb1945d85
luisricarte <luis.ricarte.ext@synchro.com.br>
2023-05-20 14:09:07 -0300

    adição cookie consent

--- sections/CookieConsent/CookieConsent.time-275.tsx
+++ sections/CookieConsent/CookieConsent.time-275.tsx
@@ -1,58 +1,51 @@
 import Button from "$store/components/ui/Button.tsx";
-import { useId } from "preact/hooks";
+import Icon from "$store/components/ui/Icon.tsx";
+export interface Props {
+  title: string,
+  middleText: string,
+  footerText: string,
+  buttonText: string,
+}
 
-const script = (id: string) => `
-const callback = () => {
-  const KEY = 'store-cookie-consent';
-  const ACCEPTED = 'accepted';
-  const HIDDEN = "translate-y-[200%]";
-  
-  const consent = localStorage.getItem(KEY);
-  const elem = document.getElementById("${id}");
-  
-  if (consent !== ACCEPTED) {
-    elem.querySelector('[data-button-cc-accept]').addEventListener('click', function () {
-      localStorage.setItem(KEY, ACCEPTED);
-      elem.classList.add(HIDDEN);
-    });
-    elem.querySelector('[data-button-cc-close]').addEventListener('click', function () {
-      elem.classList.add(HIDDEN);
-    });
-    elem.classList.remove(HIDDEN);
-  }
-};
+const script = (id: string) => ``;
 
-window.addEventListener('scroll', callback, { once: true });
-`;
-
-function CookieConsent() {
-  const id = `cookie-consent-${useId()}`;
+function CookieConsent({ title, middleText, footerText, buttonText}: Props) {
+  // const id = `cookie-consent-${useId()}`;
 
   return (
     <>
-      <div
+      {/* <div
         id={id}
         class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 w-screen z-50"
-      >
-        <div class="container px-4 py-4 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
+      > */}
+      
+        <div class="container px-3 py-3 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100 ">
+        
+          <div class="p-4 bg-gray-200 flex flex-col w-full font-serif">
+          <span class="flex-grow text-xl ">
+            {title}
+          </span>
           <span class="flex-grow text-sm">
-            Utilizamos cookies em nosso site para melhorar seu desempenho,
-            segurança e personalizar conteúdo e anúncios.
+            {middleText}
           </span>
 
           <a href="https://www.deco.cx">
             <span class="underline text-sm">
-              Nossa política de privacidade
+              {footerText}
             </span>
           </a>
+          </div>
 
-          <div class="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
-            <Button data-button-cc-accept>Aceitar</Button>
-            <Button data-button-cc-close class="btn-outline">Fechar</Button>
+          <div class="flex flex-col sm:flex-col w-full sm:w-auto ">
+            <Button data-button-cc-close class="btn-outline">
+            <Icon id="XMark" width={20} height={20} strokeWidth={2} />
+            </Button>
+            <Button class="bg-slate-700" data-button-cc-accept>{buttonText}</Button>
+            
           </div>
         </div>
-      </div>
-      <script type="module" dangerouslySetInnerHTML={{ __html: script(id) }} />
+      {/* </div> */}
+      {/* <script type="module" dangerouslySetInnerHTML={{ __html: script(id) }} /> */}
     </>
   );
 }
