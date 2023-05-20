82ffd57b64f32ea8e469cd5098732f95efa85790
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-05-20 19:27:02 -0300

    Implementing alterations

--- sections/Newsletter/Newsletter.Luminosity.tsx
+++ sections/Newsletter/Newsletter.Luminosity.tsx
@@ -1,11 +1,14 @@
 export interface NewsletterProps {
   title: string;
+  inputPlaceholder?: string;
 }
 
-export default function Newsletter({ title }: NewsletterProps) {
+export default function Newsletter(
+  { title, inputPlaceholder }: NewsletterProps,
+) {
   return (
     <div className="w-full h-screen bg-zinc-100 p-4 flex justify-center items-center">
-      <div className="w-96 lg:w-full bg-[#6D8B61] flex flex-col gap-10 p-4 lg:py-16 lg:justify-center items-center">
+      <div className="w-96 lg:w-full bg-primary flex flex-col gap-10 p-4 lg:py-16 lg:justify-center items-center">
         <header>
           <h1 className="text-3xl lg:text-5xl text-white">
             {title}
@@ -15,7 +18,7 @@ export default function Newsletter({ title }: NewsletterProps) {
           <div className="w-full flex flex-col gap-4 lg:flex-row">
             <input
               className="w-full bg-transparent text-white border border-white px-3 py-2 placeholder:text-white focus:border focus:border-white focus:outline-none"
-              placeholder="Insira seu e-mail"
+              placeholder={inputPlaceholder ?? "Insira seu e-mail"}
               type="text"
             />
             <button className="group w-fit h-10 p-2 flex justify-center items-center bg-white hover:bg-transparent hover:border hover:border-white">
