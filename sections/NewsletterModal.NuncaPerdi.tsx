74a64f71d314f8cca7e90913b33d975c6a2fe506
Lucas Tayar <lucastayar12@gmail.com>
2023-05-20 15:55:29 -0300

    Modal 2.0

--- sections/NewsletterModal.NuncaPerdi.tsx
+++ sections/NewsletterModal.NuncaPerdi.tsx
@@ -20,36 +20,48 @@ function ModalNewsLetter(
   { strings, inputLabels, checkBoxTxt }: Props,
 ) {
   return (
-    <div class="card card-side bg-base-100 shadow-xl">
-      <figure class="w-96">Imagem</figure>
-      <div class="card-body items-center">
-        <h2 class="card-title">{strings.bemvindo}</h2>
-        <p>{strings.texto}</p>
+    <>
+      <label for="my-modal-5" class="btn">Abrir Modal</label>
+      <input type="checkbox" id="my-modal-5" class="modal-toggle" checked/>
+      <label for="my-modal-5" class="modal cursor-pointer">
+        <div class="card card-side bg-base-100 shadow-xl flex flex-wrap">
+          <figure class="flex-end h-full"><img src="/NKP Modal.jpg" alt="" /></figure>
+          <div class="card-body items-center">
+            <h2 class="card-title">{strings.bemvindo}</h2>
+            <p>{strings.texto}</p>
 
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">{inputLabels.primeiroNome}</span>
-          </label>
-          <input
-            type="text"
-            placeholder={inputLabels.placeholderNome}
-            class="input input-bordered w-full max-w-xs"
-          />
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">{inputLabels.primeiroNome}</span>
+              </label>
+              <input
+                type="text"
+                placeholder={inputLabels.placeholderNome}
+                class="input input-bordered w-full max-w-xs"
+              />
 
-          <label class="label">
-            <span class="label-text">{inputLabels.email}</span>
-          </label>
-          <input
-            type="text"
-            placeholder={inputLabels.placeholderEmail}
-            class="input input-bordered w-full max-w-xs"
-          />
+              <label class="label">
+                <span class="label-text">{inputLabels.email}</span>
+              </label>
+              <input
+                type="text"
+                placeholder={inputLabels.placeholderEmail}
+                class="input input-bordered w-full max-w-xs"
+              />
+
+              <label class="label cursor-pointer">
+                <input type="checkbox" class="checkbox" />
+                <span class="label-text justify-start">{checkBoxTxt}</span>
+              </label>
+            </div>
+
+            <div class="card-actions justify-center">
+              <button class="btn">Enviar</button>
+            </div>
+          </div>
         </div>
-
-        <div class="card-actions justify-end">
-        </div>
-      </div>
-    </div>
+      </label>
+    </>
   );
 }
 
