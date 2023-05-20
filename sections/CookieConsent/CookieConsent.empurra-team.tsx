2a40e2b2086412e852dd06665f6c946b7d5c4faf
José Augusto Rufino <jose.rufino.ext@synchro.com.br>
2023-05-20 13:15:32 -0300

    Inicia dev Cookie Consent

---
+++ sections/CookieConsent/CookieConsent.empurra-team.tsx
@@ -0,0 +1,60 @@
+import Button from "$store/components/ui/Button.tsx";
+import { useId } from "preact/hooks";
+
+const script = (id: string) => `
+const callback = () => {
+  const KEY = 'store-cookie-consent';
+  const ACCEPTED = 'accepted';
+  const HIDDEN = "translate-y-[200%]";
+  
+  const consent = localStorage.getItem(KEY);
+  const elem = document.getElementById("${id}");
+  
+  if (consent !== ACCEPTED) {
+    elem.querySelector('[data-button-cc-accept]').addEventListener('click', function () {
+      localStorage.setItem(KEY, ACCEPTED);
+      elem.classList.add(HIDDEN);
+    });
+    elem.querySelector('[data-button-cc-close]').addEventListener('click', function () {
+      elem.classList.add(HIDDEN);
+    });
+    elem.classList.remove(HIDDEN);
+  }
+};
+
+window.addEventListener('scroll', callback, { once: true });
+`;
+
+function CookieConsent() {
+  const id = `cookie-consent-${useId()}`;
+
+  return (
+    <>
+      <div
+        id={id}
+        class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 w-screen z-50"
+      >
+        <div class="container px-4 py-4 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
+          <span class="flex-grow text-sm">
+            Utilizamos cookies em nosso site para melhorar seu desempenho,
+            segurança e personalizar conteúdo e anúncios.
+          </span>
+
+          <a href="https://www.deco.cx">
+            <span class="underline text-sm">
+              Nossa política de privacidade
+            </span>
+          </a>
+
+          <div class="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
+            <Button data-button-cc-accept>Aceitar</Button>
+            <Button data-button-cc-close class="btn-outline">Fechar</Button>
+          </div>
+        </div>
+      </div>
+      <script type="module" dangerouslySetInnerHTML={{ __html: script(id) }} />
+    </>
+  );
+}
+
+export default CookieConsent;
