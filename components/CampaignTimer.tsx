c63978d86009c982bdd533c1d9cb48df9c942d56
João <joao.menezes@ccc.ufcg.edu.br>
2023-05-20 14:46:14 -0300

    default values

--- components/CampaignTimer.tsx
+++ components/CampaignTimer.tsx
@@ -9,24 +9,24 @@ export interface Props {
 export default function Navbar(params: Props) {
   return (
     <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center gap-2 lg:gap-16 bg-red-300 p-4">
-      <p className="lg:text-xl text-center text-white text-sm">Time left for a campaign to end with a <a href="#" className="font-medium underline hover:no-underline">link</a></p>
+      <p className="lg:text-xl text-center text-white text-sm">Time left for a campaign to end with a <a href={params.link ?? "#"} className="font-medium underline hover:no-underline">link</a></p>
       <div className="flex justify-center lg:gap-14">
         <div className="grid grid-flow-col gap-6 p-3">
           <div className="flex flex-col items-center">
             <span className="countdown text-xl text-white">
-              <span style={{"--value":params.hours}}></span>
+              <span style={{"--value":params.hours ?? 12}}></span>
             </span>
             <a className="text-white text-xs">Hours</a>
           </div> 
           <div className="flex flex-col items-center">
             <span className="countdown font-sans text-xl text-white">
-              <span style={{"--value":params.minutes}}></span>
+              <span style={{"--value":params.minutes ?? 20}}></span>
             </span>
             <p className="text-white text-xs">Minutes</p>
           </div> 
           <div className="flex flex-col items-center">
             <span className="countdown font-sans text-xl text-white">
-              <span style={{"--value":params.seconds}}></span>
+              <span style={{"--value":params.seconds ?? 10}}></span>
             </span>
             <p className="text-white text-xs">Seconds</p>
           </div>
