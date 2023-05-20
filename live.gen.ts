a9a5c9a1e07a2070cd14f91c81758198ef39e292
Jorge Júnior <jorgesilvajunior10@gmail.com>
2023-05-20 17:39:27 -0300

    structure benefitsTitle

--- live.gen.ts
+++ live.gen.ts
@@ -19,20 +19,21 @@ import * as $$$$$9 from "./islands/SliderJS.tsx";
 import * as $$$$$10 from "./islands/WishlistButton.tsx";
 import * as $$$$$$$$0 from "./sections/BannerGrid.tsx";
 import * as $$$$$$$$1 from "./sections/BannerPLP.tsx";
-import * as $$$$$$$$2 from "./sections/Carousel.tsx";
-import * as $$$$$$$$3 from "./sections/CookieConsent/CookieConsent.123.tsx";
-import * as $$$$$$$$4 from "./sections/CookieConsent.tsx";
-import * as $$$$$$$$5 from "./sections/DesignSystem.tsx";
-import * as $$$$$$$$6 from "./sections/Features.tsx";
-import * as $$$$$$$$7 from "./sections/Footer.tsx";
-import * as $$$$$$$$8 from "./sections/Header.tsx";
-import * as $$$$$$$$9 from "./sections/Highlights.tsx";
-import * as $$$$$$$$10 from "./sections/LinkTree.tsx";
-import * as $$$$$$$$11 from "./sections/ProductDetails.tsx";
-import * as $$$$$$$$12 from "./sections/ProductShelf.tsx";
-import * as $$$$$$$$13 from "./sections/SearchResult.tsx";
-import * as $$$$$$$$14 from "./sections/WhatsApp.tsx";
-import * as $$$$$$$$15 from "./sections/WishlistGallery.tsx";
+import * as $$$$$$$$2 from "./sections/BenefitsTitle/BenefitsTitle.123.tsx";
+import * as $$$$$$$$3 from "./sections/Carousel.tsx";
+import * as $$$$$$$$4 from "./sections/CookieConsent/CookieConsent.123.tsx";
+import * as $$$$$$$$5 from "./sections/CookieConsent.tsx";
+import * as $$$$$$$$6 from "./sections/DesignSystem.tsx";
+import * as $$$$$$$$7 from "./sections/Features.tsx";
+import * as $$$$$$$$8 from "./sections/Footer.tsx";
+import * as $$$$$$$$9 from "./sections/Header.tsx";
+import * as $$$$$$$$10 from "./sections/Highlights.tsx";
+import * as $$$$$$$$11 from "./sections/LinkTree.tsx";
+import * as $$$$$$$$12 from "./sections/ProductDetails.tsx";
+import * as $$$$$$$$13 from "./sections/ProductShelf.tsx";
+import * as $$$$$$$$14 from "./sections/SearchResult.tsx";
+import * as $$$$$$$$15 from "./sections/WhatsApp.tsx";
+import * as $$$$$$$$16 from "./sections/WishlistGallery.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -174,21 +175,23 @@ const manifest = {
     "$live/sections/UseSlot.tsx": i2$$$$2,
     "deco-sites/fashion/sections/BannerGrid.tsx": $$$$$$$$0,
     "deco-sites/fashion/sections/BannerPLP.tsx": $$$$$$$$1,
-    "deco-sites/fashion/sections/Carousel.tsx": $$$$$$$$2,
-    "deco-sites/fashion/sections/CookieConsent.tsx": $$$$$$$$4,
+    "deco-sites/fashion/sections/BenefitsTitle/BenefitsTitle.123.tsx":
+      $$$$$$$$2,
+    "deco-sites/fashion/sections/Carousel.tsx": $$$$$$$$3,
+    "deco-sites/fashion/sections/CookieConsent.tsx": $$$$$$$$5,
     "deco-sites/fashion/sections/CookieConsent/CookieConsent.123.tsx":
-      $$$$$$$$3,
-    "deco-sites/fashion/sections/DesignSystem.tsx": $$$$$$$$5,
-    "deco-sites/fashion/sections/Features.tsx": $$$$$$$$6,
-    "deco-sites/fashion/sections/Footer.tsx": $$$$$$$$7,
-    "deco-sites/fashion/sections/Header.tsx": $$$$$$$$8,
-    "deco-sites/fashion/sections/Highlights.tsx": $$$$$$$$9,
-    "deco-sites/fashion/sections/LinkTree.tsx": $$$$$$$$10,
-    "deco-sites/fashion/sections/ProductDetails.tsx": $$$$$$$$11,
-    "deco-sites/fashion/sections/ProductShelf.tsx": $$$$$$$$12,
-    "deco-sites/fashion/sections/SearchResult.tsx": $$$$$$$$13,
-    "deco-sites/fashion/sections/WhatsApp.tsx": $$$$$$$$14,
-    "deco-sites/fashion/sections/WishlistGallery.tsx": $$$$$$$$15,
+      $$$$$$$$4,
+    "deco-sites/fashion/sections/DesignSystem.tsx": $$$$$$$$6,
+    "deco-sites/fashion/sections/Features.tsx": $$$$$$$$7,
+    "deco-sites/fashion/sections/Footer.tsx": $$$$$$$$8,
+    "deco-sites/fashion/sections/Header.tsx": $$$$$$$$9,
+    "deco-sites/fashion/sections/Highlights.tsx": $$$$$$$$10,
+    "deco-sites/fashion/sections/LinkTree.tsx": $$$$$$$$11,
+    "deco-sites/fashion/sections/ProductDetails.tsx": $$$$$$$$12,
+    "deco-sites/fashion/sections/ProductShelf.tsx": $$$$$$$$13,
+    "deco-sites/fashion/sections/SearchResult.tsx": $$$$$$$$14,
+    "deco-sites/fashion/sections/WhatsApp.tsx": $$$$$$$$15,
+    "deco-sites/fashion/sections/WishlistGallery.tsx": $$$$$$$$16,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$3,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$4,
     "deco-sites/std/sections/configOCC.global.tsx": i2$$$$5,
---
+++ sections/BenefitsTitle/BenefitsTitle.123.tsx
@@ -0,0 +1,61 @@
+export interface props {
+ title: string;
+ description: string;
+ itemTitle: string;
+ itemDescription: string;
+}
+
+function BenefitsTitle({ title, description, itemTitle, itemDescription }: props) {
+
+ return (
+  <>
+   <div class="row container flex flex-col items-center text-center px-5">
+    <div class="text-center">
+     <h1 class="text-4xl">Benefits title</h1>
+     <span class="text-lg">description benefits</span>
+    </div>
+
+    <div class="grid grid-cols-4 gap-4 divide-x-2">
+     <div class="flex items-center justify-start gap-2">
+      <div>
+      </div>
+      <div>
+       <h3>Title item</h3>
+       <p>description of the item</p>
+      </div>
+     </div>
+
+     <div class="flex items-center justify-start gap-2">
+      <div>
+       <img></img>
+      </div>
+      <div>
+       <h3>Title item</h3>
+       <p>description of the item</p>
+      </div>
+     </div>
+
+     <div class="flex items-center justify-start gap-2">
+      <div></div>
+      <div>
+       <h3>Title item</h3>
+       <p>description of the item</p>
+      </div>
+     </div>
+
+     <div class="flex items-center justify-start gap-2">
+      <div></div>
+      <div>
+       <h3>Title item</h3>
+       <p>description of the item</p>
+      </div>
+     </div>
+    </div>
+   </div>
+
+  </>
+ )
+
+}
+
+export default BenefitsTitle 
