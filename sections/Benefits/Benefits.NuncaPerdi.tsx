417465bac33776c8025efc16e6d33e782bc9df22
Lucas Tayar <lucastayar12@gmail.com>
2023-05-20 17:15:14 -0300

    Bug git part 3

--- sections/Benefits/Benefits.NuncaPerdi.tsx
+++ sections/Benefits/Benefits.NuncaPerdi.tsx
@@ -2,12 +2,12 @@ export type BenefitItem = {
   icon: { src: string; alt: string };
   title: string;
   description: string;
-}
+};
 
 export type HeaderItem = {
   title: string;
   description: string;
-}
+};
 
 export interface Props {
   benefit: BenefitItem;
@@ -19,30 +19,30 @@ function Benefit(
 ) {
   return (
     <div class="hero min-h-screen bg-base-200">
-
       <div class="hero-content text-center">
-        <h1> {header.title} </h1>
-        <p> {header.description} </p>
+        <h1>{header.title}</h1>
+        <p>{header.description}</p>
       </div>
 
       <div class="hero-content flex w-full text-center">
-
         <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
-          <figure><img src={benefit.icon.src} alt={benefit.icon.alt} /></figure>
-          <h3> {benefit.title} </h3>
-          <p> {benefit.description} </p>
+          <figure>
+            <img src={benefit.icon.src} alt={benefit.icon.alt} />
+          </figure>
+          <h3>{benefit.title}</h3>
+          <p>{benefit.description}</p>
         </div>
 
         <div class="divider divider-horizontal" />
 
         <div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
-          <figure><img src={benefit.icon.src} alt={benefit.icon.alt} /></figure>
-          <h3> {benefit.title} </h3>
-          <p> {benefit.description} </p>
+          <figure>
+            <img src={benefit.icon.src} alt={benefit.icon.alt} />
+          </figure>
+          <h3>{benefit.title}</h3>
+          <p>{benefit.description}</p>
         </div>
-
       </div>
-
     </div>
   );
 }
