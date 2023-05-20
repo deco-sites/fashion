0c5fef89d28c5864711c3dde7cb9ce171f010fd4
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-20 17:31:56 -0300

    refactor: add default properties

--- sections/Benefits/Benefits.Code-Crafters.tsx
+++ sections/Benefits/Benefits.Code-Crafters.tsx
@@ -10,7 +10,7 @@ export interface Props {
   }[];
 }
 
-export default function Benefits({ title, description, items }: Props) {
+export default function Benefits({ title = "Benefícios", description = "Descrição", items }: Props) {
   return (
     <div class="p-10">
       <div class="text-center">
@@ -20,15 +20,18 @@ export default function Benefits({ title, description, items }: Props) {
       <div class="container min-h-[280px] p-6 sm:px-0 sm:py-10">
         <div class="border-base-200 border border-solid">
           <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
-          {items.map(({ icon: id = "Deco", title, description }) => (
-            <div class="flex flex-row gap-4 items-center py-6 sm:flex-col sm:py-0 sm:px-8" key={title}>
-              <Icon id={id} width={40} height={40} strokeWidth={1} />
-              <div class="text-center">
-                <h3 class="text-xl">{title}</h3>
-                <p class="text-sm text-neutral-400">{description}</p>
+            {items.map(({ icon: id = "Deco", title="Título", description = "Descrição" }) => (
+              <div
+                class="flex flex-row gap-4 items-center py-6 sm:flex-col sm:py-0 sm:px-8"
+                key={title}
+              >
+                <Icon id={id} width={40} height={40} strokeWidth={1} />
+                <div class="text-center">
+                  <h3 class="text-xl">{title}</h3>
+                  <p class="text-sm text-neutral-400">{description}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
           </div>
         </div>
       </div>
