025b6867c21cfec215e12ccee36e264db96c7500
Rodrigo Lima <rodrigo@capyba.com>
2023-05-20 16:03:31 -0300

    Apply spacing and font styling

--- sections/DiscountBanner/DiscountBanner.capyvarias.tsx
+++ sections/DiscountBanner/DiscountBanner.capyvarias.tsx
@@ -9,11 +9,11 @@ export default function DiscountBanner(
   return (
     <div class="bg-primary w-full">
       <div
-        class="flex gap-4 w-max animate-[slideleft_10s_linear_infinite]"
+        class="flex gap-10 w-max text-sm animate-[slideleft_10s_linear_infinite]"
         style={`animation-duration: ${slideTimeInSeconds}s;`}
       >
         {messages.map((message, index) => (
-          <div class="pt-1 pb-1 md:pt-2 md:pb-2 flex gap-4 items-center text-primary-content group">
+          <div class="pt-2 pb-2 lg:pt-4 lg:pb-4 flex gap-10 items-center text-primary-content group">
             <p>{message}</p>
             <span class="bg-primary-content h-1 w-1 rounded group-last:hidden">
             </span>
