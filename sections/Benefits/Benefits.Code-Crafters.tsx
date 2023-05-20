1ca6dc08f76679ec92965ab644f23e7240a59a25
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-20 17:05:47 -0300

    style: finalize Benefits component stylization

--- sections/Benefits/Benefits.Code-Crafters.tsx
+++ sections/Benefits/Benefits.Code-Crafters.tsx
@@ -1,4 +1,4 @@
-import Icon, {AvailableIcons} from '$store/components/ui/Icon.tsx';
+import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
 
 export interface Props {
   title: string;
@@ -10,22 +10,28 @@ export interface Props {
   }[];
 }
 
-export default function Benefits({title, description, items}: Props) {
+export default function Benefits({ title, description, items }: Props) {
   return (
-    <div class="bg-gray-500 p-10">
-    <div class="text-center bg-red-400">
-      <h1 class="text-4xl">{title}</h1>
-      <p>{description}</p>
-    </div>
-    <div class="stats stats-vertical lg:stats-horizontal shadow">
-      {items.map(({icon: id = "Deco", title, description}) => (
-        <div class="stat" key={title}>
-          <Icon id={id} width={40} height={40} strokeWidth={1} />
-          <h3>{title}</h3>
-          <p>{description}</p>
+    <div class="p-10">
+      <div class="text-center">
+        <h1 class="text-4xl">{title}</h1>
+        <p class="text-2xl text-neutral-400">{description}</p>
+      </div>
+      <div class="container min-h-[280px] p-6 sm:px-0 sm:py-10">
+        <div class="border-base-200 border border-solid">
+          <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
+          {items.map(({ icon: id = "Deco", title, description }) => (
+            <div class="flex flex-row gap-4 items-center py-6 sm:flex-col sm:py-0 sm:px-8" key={title}>
+              <Icon id={id} width={40} height={40} strokeWidth={1} />
+              <div class="text-center">
+                <h3 class="text-xl">{title}</h3>
+                <p class="text-sm text-neutral-400">{description}</p>
+              </div>
+            </div>
+          ))}
+          </div>
         </div>
-      ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
