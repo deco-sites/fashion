21c290f7cfecb5b87f9c2ec72ead42d2a6e0e649
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-05-20 18:09:36 -0300

    feat finalizado css da versão mobile

--- sections/Timers/CampaignTimer.Edubias.tsx
+++ sections/Timers/CampaignTimer.Edubias.tsx
@@ -1,34 +1,39 @@
 export interface Props {
   title: string;
-  hours?: number;
-  minutes?: number;
-  seconds: number;
+  hours?: string;
+  minutes?: string;
+  seconds: string;
+  textButton: string;
 }
 
 export default function CampaignTimer(
-  { title, hours = 0, minutes = 0, seconds = 0 }: Props,
+  { title, hours = '00', minutes = '00', seconds, textButton }: Props,
 ) {
   return (
-    <div className="h-28 p-4 bg-red-400 flex flex-col justify-center items-center text-white">
-      <h2 className="text-sm mb-4">{title}</h2>
-      <div className="flex">
-        <div className="flex">
-          <div>
-            <p>{hours}</p>
-            <p>Hours</p>
+    <div class="h-28 py-4 px-16 w-screen bg-accent flex flex-col justify-center
+      items-center text-accent-content gap-4">
+      <h2 class="text-sm leading-5">{title}</h2>
+      <div class="flex gap-6">
+        <div class="flex gap-2 items-center">
+          <div class="flex flex-col items-center">
+            <p class="text-xl leading-7">{hours}</p>
+            <p class="text-xs leading-4">Hours</p>
           </div>
-          <p>:</p>
-          <div>
-            <p>{minutes}</p>
-            <p>Minutes</p>
+          <p class="text-xl leading-7">:</p>
+          <div class="flex flex-col items-center">
+            <p class="text-xl leading-7">{minutes}</p>
+            <p class="text-xs leading-4">Minutes</p>
           </div>
-          <p>:</p>
-          <div>
-            <p>{seconds}</p>
-            <p>Seconds</p>
+          <p class="text-xl leading-7">:</p>
+          <div class="flex flex-col items-center">
+            <p class="text-xl leading-7">{seconds}</p>
+            <p class="text-xs leading-4">Seconds</p>
           </div>
         </div>
-        <button>Button</button>
+        <button class="flex items-center gap-2 py-2 px-3 justify-center bg-primary
+          text-primary-content leading-6">
+          {textButton}
+        </button>
       </div>
     </div>
   );
