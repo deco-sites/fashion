4937054e1a8239ec4a0539c332eb62fd2c0401d5
Pedro Lima <pedro.lima@ccc.ufcg.edu.br>
2023-05-20 16:09:52 -0300

    Revert "Adding Features section"
    This reverts commit 04c79327a5fc8f1acfd5d9d9cdca5c416ac39d8e.

--- components/ui/Features.tsx
+++ components/ui/Features.tsx
@@ -1,4 +1,6 @@
-import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import Icon, {
+  AvailableIcons,
+} from "$store/components/ui/Icon.tsx";
 
 export interface Feature {
   /**
@@ -16,29 +18,18 @@ export interface Feature {
 }
 
 export interface Props {
-  benefitsTitle: string;
-  benefitsDescription: string;
   features: Feature[];
 }
 
 function FeatureHighlights(
-  {
-    features,
-    benefitsTitle = "Benefits Title",
-    benefitsDescription =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at erat non erat accumsan vulputate a eu tellus. Cras dapibus nibh quis blandit vulputate. Nunc rutrum sapien risus, non feugiat nulla efficitur at. Duis volutpat vel felis et consectetur. ",
-  }: Props,
+  { features }: Props,
 ) {
   return (
     <div class="container min-h-[280px] p-6 sm:px-0 sm:py-10">
-      <div class="flex flex-col items-center w-full sm:w-auto">
-        <span class="font-medium text-2xl text-center">{benefitsTitle}</span>
-        <span class="text-center">{benefitsDescription}</span>
-      </div>
-      <div>
+      <div class="border-base-200 border border-solid">
         <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
           {features.map(({ icon: id = "Truck", title, description }) => (
-            <div class="flex flex-row gap-4 py-6 sm:flex-row sm:py-0 sm:px-8">
+            <div class="flex flex-row gap-4 py-6 sm:flex-col sm:py-0 sm:px-8">
               <Icon
                 id={id}
                 width={40}
