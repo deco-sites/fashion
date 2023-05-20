b00502aed1038872f0db2add030733b61a71473d
Kleber <kleberst88@gmail.com>
2023-05-20 16:33:52 -0300

    feat: Criação do componente de logos

---
+++ sections/Logos/Logos.KleberTavares.tsx
@@ -0,0 +1,39 @@
+import type { Image as ImageType } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
+
+export interface Props {
+  title: string;
+  subtitle: string;
+  imgUrl: Array<{
+    url: ImageType;
+    textAlt: string;
+  }>;
+}
+
+export default function Logos({
+  imgUrl,
+  title,
+  subtitle,
+}: Props) {
+  return (
+    <div className="w-full">
+      <div className="text-center py-10">
+        <h1 className="text-4xl">{title}</h1>
+        <h2 className="px-14">{subtitle}</h2>
+      </div>
+      <div className="carousel px-auto py-2 flex flex-row justify-center">
+        {imgUrl?.map((element) => (
+          <div className="carousel-item">
+            <Image
+              className="object-contain mx-4"
+              src={element.url}
+              alt={element.textAlt}
+              width={150}
+              height={150}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
