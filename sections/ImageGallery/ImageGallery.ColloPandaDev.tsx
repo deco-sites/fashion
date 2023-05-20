fe614c584670179c330e5914c24a9f8de5c59065
Rafael_Oliveira7 <rafael.miranda@rocky.ag>
2023-05-20 18:47:01 -0300

    Feat: ImageGallery

--- sections/ImageGallery/ImageGallery.ColloPandaDev.tsx
+++ sections/ImageGallery/ImageGallery.ColloPandaDev.tsx
@@ -8,15 +8,15 @@ export interface Banner {
    * @description Image alt text
    */
   alt: string;
-    /**
+  /**
    * @description Adicione um link
    */
-    href: string;
+  href: string;
 }
 export interface DivColumn {
-    bannerTop: Banner;
-    bannerBottom: Banner;
-  }
+  bannerTop: Banner;
+  bannerBottom: Banner;
+}
 export type BorderRadius =
   | "none"
   | "sm"
@@ -75,21 +75,21 @@ const RADIUS_DESKTOP = {
   "full": "sm:rounded-full",
 };
 const LAYOUT_MOBILE = {
-    "Assimétrico": "w-full h-full",
-    "Simétrico": "w-full aspect-square max-w-[216px]"
-}
+  "Assimétrico": "w-full h-full",
+  "Simétrico": "w-full aspect-square max-w-[216px]",
+};
 const LAYOUT_DESKTOP = {
-    "Grid": "sm:w-full sm:h-full sm:max-h-full sm:max-w-full",
-    "Inline": "sm:w-[25vw] sm:h-[100%] sm:max-h-full sm:max-w-full"
-}
+  "Grid": "sm:w-full sm:h-full sm:max-h-full sm:max-w-full",
+  "Inline": "sm:w-[25vw] sm:h-[100%] sm:max-h-full sm:max-w-full",
+};
 const LAYOUT_MOBILEv2 = {
-    "Assimétrico": "",
-    "Simétrico": "w-full"
-}
+  "Assimétrico": "",
+  "Simétrico": "w-full",
+};
 const LAYOUT_DESKTOPv2 = {
-    "Grid": "sm:max-h-none sm:max-w-none",
-    "Inline": "sm:flex-row"
-}
+  "Grid": "sm:max-h-none sm:max-w-none",
+  "Inline": "sm:flex-row",
+};
 export default function BannnerGrid({
   title,
   descricao,
@@ -99,73 +99,85 @@ export default function BannnerGrid({
 }: Props) {
   return (
     <section class="container w-full px-4 mx-auto md:px-10">
-        <div class="py-6 md:py-0 md:pb-[40px] flex items-center mt-6 flex-col">
-            <h2 class="text-[36px] font-normal lg:text-[60px]">
-                {title}
-            </h2>
-            <p class="text-center font-normal text-2xl">{descricao}</p>
-        </div>
-        <div class="flex w-full gap-4">
-            {divColumn.map(({ bannerTop, bannerBottom}) => (
-                <div class={`flex flex-col gap-4 ${LAYOUT_MOBILEv2[SelectLayout.mobile ?? "Assimétrico"]} ${LAYOUT_DESKTOPv2[SelectLayout.desktop ?? "Grid"]}`}>
-                    <a
-                        href={bannerTop.href}
-                        class={`overflow-hidden ${
-                        RADIUS_MOBILE[borderRadius.mobile ?? "none"]
-                        } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
-                    >
-                    <Picture>
-                    <Source
-                        media="(max-width: 767px)"
-                        src={bannerTop.srcMobile}
-                        width= {100}
-                    />
-                    <Source
-                        media="(min-width: 768px)"
-                        src={bannerTop.srcDesktop ? bannerTop.srcDesktop : bannerTop.srcMobile}
-                        width={250}
-                    />
-                    <img
-                        class={` ${LAYOUT_MOBILE[SelectLayout.mobile ?? "Assimétrico"]} ${LAYOUT_DESKTOP[SelectLayout.desktop ?? "Grid"]}`}
-                        src={bannerTop.srcMobile}
-                        alt={bannerTop.alt}
-                        decoding="async"
-                        loading="lazy"
-                    />
-                    </Picture>
-                    </a>
-                    <a
-                        href={bannerBottom.href}
-                        class={`overflow-hidden ${
-                        RADIUS_MOBILE[borderRadius.mobile ?? "none"]
-                        } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
-                    >
-                    <Picture>
-                    <Source
-                        media="(max-width: 767px)"
-                        src={bannerBottom.srcMobile}
-                        width= {100}
-                        className={"w-full h-auto"}
-                    />
-                    <Source
-                        media="(min-width: 768px)"
-                        src={bannerBottom.srcDesktop ? bannerBottom.srcDesktop : bannerBottom.srcMobile}
-                        width={250}
-                        className={"w-full h-auto"}
-                    />
-                    <img
-                        class={` ${LAYOUT_MOBILE[SelectLayout.mobile ?? "Assimétrico"]} ${LAYOUT_DESKTOP[SelectLayout.desktop ?? "Grid"]}`}
-                        sizes="(max-width: 640px) 100vw, 30vw"
-                        src={bannerBottom.srcMobile}
-                        alt={bannerBottom.alt}
-                        decoding="async"
-                        loading="lazy"
-                    />
-                    </Picture>
-                    </a>
-                </div>
-            ))}
-        </div>
+      <div class="py-6 md:py-0 md:pb-[40px] flex items-center mt-6 flex-col">
+        <h2 class="text-[36px] font-normal lg:text-[60px]">
+          {title}
+        </h2>
+        <p class="text-center font-normal text-2xl">{descricao}</p>
+      </div>
+      <div class="flex w-full gap-4">
+        {divColumn.map(({ bannerTop, bannerBottom }) => (
+          <div
+            class={`flex flex-col gap-4 ${
+              LAYOUT_MOBILEv2[SelectLayout.mobile ?? "Assimétrico"]
+            } ${LAYOUT_DESKTOPv2[SelectLayout.desktop ?? "Grid"]}`}
+          >
+            <a
+              href={bannerTop.href}
+              class={`overflow-hidden ${
+                RADIUS_MOBILE[borderRadius.mobile ?? "none"]
+              } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
+            >
+              <Picture>
+                <Source
+                  media="(max-width: 767px)"
+                  src={bannerTop.srcMobile}
+                  width={100}
+                />
+                <Source
+                  media="(min-width: 768px)"
+                  src={bannerTop.srcDesktop
+                    ? bannerTop.srcDesktop
+                    : bannerTop.srcMobile}
+                  width={250}
+                />
+                <img
+                  class={` ${
+                    LAYOUT_MOBILE[SelectLayout.mobile ?? "Assimétrico"]
+                  } ${LAYOUT_DESKTOP[SelectLayout.desktop ?? "Grid"]}`}
+                  src={bannerTop.srcMobile}
+                  alt={bannerTop.alt}
+                  decoding="async"
+                  loading="lazy"
+                />
+              </Picture>
+            </a>
+            <a
+              href={bannerBottom.href}
+              class={`overflow-hidden ${
+                RADIUS_MOBILE[borderRadius.mobile ?? "none"]
+              } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
+            >
+              <Picture>
+                <Source
+                  media="(max-width: 767px)"
+                  src={bannerBottom.srcMobile}
+                  width={100}
+                  className={"w-full h-auto"}
+                />
+                <Source
+                  media="(min-width: 768px)"
+                  src={bannerBottom.srcDesktop
+                    ? bannerBottom.srcDesktop
+                    : bannerBottom.srcMobile}
+                  width={250}
+                  className={"w-full h-auto"}
+                />
+                <img
+                  class={` ${
+                    LAYOUT_MOBILE[SelectLayout.mobile ?? "Assimétrico"]
+                  } ${LAYOUT_DESKTOP[SelectLayout.desktop ?? "Grid"]}`}
+                  sizes="(max-width: 640px) 100vw, 30vw"
+                  src={bannerBottom.srcMobile}
+                  alt={bannerBottom.alt}
+                  decoding="async"
+                  loading="lazy"
+                />
+              </Picture>
+            </a>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
