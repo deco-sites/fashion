1016af8667ffe3ef5181fcc0b8bce6f2ecf53fbe
Jaqueline Silva <jscnewlf@gmail.com>
2023-05-20 13:59:37 -0300

    Criação estrutura inicial FAQ
    Subindo esqueleto html e componente dropdown pro faq

---
+++ sections/FAQ/FAQ.KATIAU.tsx
@@ -0,0 +1,41 @@
+export interface Props {
+    questions: Array<{
+      question: string;
+      answer: string;
+    }>;
+  }
+  
+  export default function FAQ({ questions }: Props) {
+    return (
+      <div class="container-faq">
+          
+          <div class="top">
+              <h1>FAQs</h1>
+              <p></p>
+          </div>
+          
+  
+          <div class="middle">
+           {questions.map(({ question, answer }) => 
+              <div tabIndex={0} class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box" >
+                <input type="checkbox" />
+                <div class="collapse-title text-xl font-medium">
+                  {question}
+                </div>
+                <div class="collapse-content"> 
+                  <p>{answer}</p>
+                </div>
+              </div>
+           )}
+          </div>
+  
+          <div class="bottom">
+              <h3>Still have a question?</h3>
+              <p></p>
+              <button className="btn">Button</button>
+          </div>
+  
+      </div>
+  
+      );
+  }
