1783ca3b9667bd58dcb8d64018471de09f94dde5
ezequiasOR <ezequias.rocha@ccc.ufcg.edu.br>
2023-05-20 14:33:16 -0300

    WIP

--- sections/CookieConsent/CookieConsent.time-275.tsx
+++ sections/CookieConsent/CookieConsent.time-275.tsx
@@ -1,27 +1,27 @@
 import Button from "$store/components/ui/Button.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
+
 export interface Props {
-  title: string,
-  middleText: string,
-  footerText: string,
-  buttonText: string,
+  title: string;
+  middleText: string;
+  footerText: string;
+  buttonText: string;
 }
 
-const script = (id: string) => ``;
-
-function CookieConsent({ title, middleText, footerText, buttonText}: Props) {
+function CookieConsent({ title, middleText, footerText, buttonText }: Props) {
   // const id = `cookie-consent-${useId()}`;
 
   return (
     <>
-      {/* <div
+      {
+        /* <div
         id={id}
         class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 w-screen z-50"
-      > */}
-      
-        <div class="container px-3 py-3 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100 ">
-        
-          <div class="p-4 bg-gray-200 flex flex-col w-full font-serif">
+      > */
+      }
+
+      <div class="container px-3 py-3 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100 ">
+        <div class="p-4 bg-gray-200 flex flex-col w-full font-serif">
           <span class="flex-grow text-xl ">
             {title}
           </span>
@@ -29,21 +29,34 @@ function CookieConsent({ title, middleText, footerText, buttonText}: Props) {
             {middleText}
           </span>
 
-          <a href="https://www.deco.cx">
+          <a
+            href="https://www.deco.cx"
+            class="flex items-center text-green-800"
+          >
             <span class="underline text-sm">
               {footerText}
             </span>
+            <Icon
+              id="ChevronRight"
+              size={14}
+              stroke="currentColor"
+              strokeWidth={2}
+            />
           </a>
-          </div>
-
-          <div class="flex flex-col sm:flex-col w-full sm:w-auto ">
-            <Button data-button-cc-close class="btn-outline">
-            <Icon id="XMark" width={20} height={20} strokeWidth={2} />
-            </Button>
-            <Button class="bg-slate-700" data-button-cc-accept>{buttonText}</Button>
-            
-          </div>
         </div>
+
+        <div class="flex flex-col sm:flex-col w-full sm:w-auto justify-end">
+          <Button
+            data-button-cc-close
+            class="btn-outline hover:bg-transparent p-2"
+          >
+            <Icon id="XMark" width={20} height={20} strokeWidth={2} />
+          </Button>
+          <Button class="bg-slate-700" data-button-cc-accept>
+            {buttonText}
+          </Button>
+        </div>
+      </div>
       {/* </div> */}
       {/* <script type="module" dangerouslySetInnerHTML={{ __html: script(id) }} /> */}
     </>
