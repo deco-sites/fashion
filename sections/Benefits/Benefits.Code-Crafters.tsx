8b3ef383cd1c445d39bbcf990411dd602f393285
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-20 19:34:13 -0300

    style: removes min-height from items container

--- sections/Benefits/Benefits.Code-Crafters.tsx
+++ sections/Benefits/Benefits.Code-Crafters.tsx
@@ -10,17 +10,25 @@ export interface Props {
   }[];
 }
 
-export default function Benefits({ title = "Benefícios", description = "Descrição", items }: Props) {
+export default function Benefits(
+  { title = "Benefícios", description = "Descrição", items }: Props,
+) {
   return (
     <div class="p-10">
       <div class="text-center">
         <h1 class="text-4xl">{title}</h1>
         <p class="text-2xl text-neutral-400">{description}</p>
       </div>
-      <div class="container min-h-[280px] p-6 sm:px-0 sm:py-10">
+      <div class="container p-6 sm:px-0 sm:py-10">
         <div class="border-base-200 border border-solid">
           <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
-            {items.map(({ icon: id = "Deco", title="Título", description = "Descrição" }) => (
+            {items.map((
+              {
+                icon: id = "Deco",
+                title = "Título",
+                description = "Descrição",
+              },
+            ) => (
               <div
                 class="flex flex-row gap-4 items-center py-6 sm:flex-col sm:py-0 sm:px-8"
                 key={title}
