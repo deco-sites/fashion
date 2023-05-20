120b07ea23f14d378beefef7c00d3e685a7ae007
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-20 18:58:27 -0300

    feat: add props e  variáveis

--- sections/CategoryList/CategoryList.time91.tsx
+++ sections/CategoryList/CategoryList.time91.tsx
@@ -1,7 +1,20 @@
-import Carousel from "../Carousel.tsx";
-import ProductShelf from "../ProductShelf.tsx";
+// import Carousel from "../Carousel.tsx";
+import Image from "deco-sites/std/components/Image.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
-function CategoryList() {
+export interface Category {
+    src: LiveImage;
+    alt: string;
+    href: string;
+    label: string;
+  }
+  
+  export interface Props {
+    categoryList: Category[];
+  }
+
+function CategoryList({ categoryList = [] }: Props) {
   return (
     <section className="flex justify-evenly flex-col items-center w-full h-96 py-10 px-16 text-trueGray-800">
       <div className="flex justify-center flex-col items-center">
@@ -9,7 +22,32 @@ function CategoryList() {
         <span className="text-lg">Description</span>
       </div>
       <div className="carousel flex h-80 border-trueGray-400">
-        <Carousel />
+        <Slider class="carousel carousel-center sm:carousel-end gap-6">
+        {categoryList.map(({ href, src, alt, label }, index) => (
+          <Slider.Item
+            index={index}
+            class="carousel-item first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0 min-w-[190px]"
+          >
+            <a href={href} class="card card-compact bg-base-100">
+              <figure>
+                <Image
+                  class="rounded-[40px]"
+                  src={src}
+                  alt={alt}
+                  height={664}
+                  width={308}
+                />
+              </figure>
+              <div class="card-body items-center">
+                <h2 class="card-title text-base font-medium">{label}</h2>
+              </div>
+            </a>
+          </Slider.Item>
+        ))}
+      </Slider>
+      {/* todo implementar o <Carousel/>*/}
+
+
       </div>
       <button className="btn btn-outline rounded-none w-80 h-9 flex normal-case text-base text-green-900
 ">
