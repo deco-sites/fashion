610c53d58a84c99f2a10d4772e0d17480210134c
José Augusto Rufino <jose.rufino.ext@synchro.com.br>
2023-05-20 16:25:24 -0300

    Adiciona informações do Cookie Consent

--- sections/CookieConsent/CookieConsent.empurra-team.tsx
+++ sections/CookieConsent/CookieConsent.empurra-team.tsx
@@ -1,58 +1,37 @@
 import Button from "$store/components/ui/Button.tsx";
-import { useId } from "preact/hooks";
+import Icon from "$store/components/ui/Icon.tsx";
 
-const script = (id: string) => `
-const callback = () => {
-  const KEY = 'store-cookie-consent';
-  const ACCEPTED = 'accepted';
-  const HIDDEN = "translate-y-[200%]";
-  
-  const consent = localStorage.getItem(KEY);
-  const elem = document.getElementById("${id}");
-  
-  if (consent !== ACCEPTED) {
-    elem.querySelector('[data-button-cc-accept]').addEventListener('click', function () {
-      localStorage.setItem(KEY, ACCEPTED);
-      elem.classList.add(HIDDEN);
-    });
-    elem.querySelector('[data-button-cc-close]').addEventListener('click', function () {
-      elem.classList.add(HIDDEN);
-    });
-    elem.classList.remove(HIDDEN);
-  }
-};
+export interface Props {
+  title: string;
+}
 
-window.addEventListener('scroll', callback, { once: true });
-`;
-
-function CookieConsent() {
-  const id = `cookie-consent-${useId()}`;
-
+function CookieConsent({ title }: Props) {
   return (
     <>
-      <div
-        id={id}
-        class="transform-gpu translate-y-[200%] transition fixed bottom-0 sm:bottom-4 w-screen z-50"
-      >
-        <div class="container px-4 py-4 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
-          <span class="flex-grow text-sm">
-            Utilizamos cookies em nosso site para melhorar seu desempenho,
-            segurança e personalizar conteúdo e anúncios.
-          </span>
-
+      <div class="container px-4 py-4 rounded border border-base-200 flex flex-col sm:flex-row gap-4 items-start sm:items-center shadow bg-base-100">
+        <h1>
+          <strong>Cookie policy</strong>
+          <p class="text-center mt-auto">
+            We use third-party cookies order to personalize your experience.
+          </p>
           <a href="https://www.deco.cx">
-            <span class="underline text-sm">
-              Nossa política de privacidade
+            <span class="underline text-sm text-[#6D8B61]">
+              Read our cookie policy
+              <Icon
+                id="ChevronRight"
+                size={14}
+                stroke="currentColor"
+                strokeWidth={2}
+              />
             </span>
           </a>
+        </h1>
 
-          <div class="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
-            <Button data-button-cc-accept>Aceitar</Button>
-            <Button data-button-cc-close class="btn-outline">Fechar</Button>
-          </div>
+        <div class="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
+          <Button data-button-cc-accept>Aceitar</Button>
+          <Button data-button-cc-close class="btn-outline">Fechar</Button>
         </div>
       </div>
-      <script type="module" dangerouslySetInnerHTML={{ __html: script(id) }} />
     </>
   );
 }
