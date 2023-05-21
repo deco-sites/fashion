822264dbc9182f62b1012c0d9de30a4a99cfd747
Samuel Lucas de Almeida <83282001+Samuellucas007@users.noreply.github.com>
2023-05-21 04:08:49 -0300

    Update login.tsx

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -7,16 +7,16 @@ export interface Props {
 
 function Auth({ description, linkSignIn }: Props) {
   return (
-    <div class="h-screen w-full font-sans bg-[#e6e6e6] flex flex-col items-center justify-center">
+    <div class="h-screen w-full font-sans bg-zinc-200 flex flex-col items-center justify-center">
       <div class="bg-white flex flex-col items-center justify-center p-16 w-[600px] h-[668px] shadow-md m-x-[420px] max-sm:h-full max-sm:w-full">
         <form class="flex items-center justify-center w-full flex-col gap-6">
-          <h1 class="text-[#292929] text-[48px]">Log in</h1>
-          <h3 class="text-[#292929] text-[18px]r">{description || "Lorem ipsum dolor sit amet consectetur adipisicing elit."}</h3>
+          <h1 class="text-zinc-800 text-5xl">Log in</h1>
+          <h3 class="text-zinc-800 text-lg">{description || "Lorem ipsum dolor sit amet consectetur adipisicing elit."}</h3>
 
           <div class="flex flex-col gap-y-2 w-full">
             <label htmlFor="">E-mail</label>
             <input
-              class="border border-[#AAAAAA] shadow-sm h-10"
+              class="border border-zinc-400 shadow-sm h-10"
               type="email"
               name="email"
               required
@@ -26,17 +26,17 @@ function Auth({ description, linkSignIn }: Props) {
           <div class="flex flex-col gap-y-2 w-full">
             <label htmlFor="">Password</label>
             <input
-              class="border border-[#AAAAAA] shadow-sm h-10"
+              class="border border-zinc-400 shadow-sm h-10"
               type="password"
               name="password"
               required
             />
           </div>
 
-          <button class="bg-[#273746] font-sans text-[16px] text-white h-10 w-full">
+          <button class="bg-gray-800 font-sans text-xs text-white h-10 w-full">
             Sign Up
           </button>
-          <button class="flex items-center justify-center bg-[#FFFFFF] border border-[#6D8B61] font-sans text-[16px] text-[#546F4A] h-10 w-full gap-2 focus:border-[#546F4A]">
+          <button class="flex items-center justify-center bg-white border border-lime-700 font-sans text-xs text-lime-800 h-10 w-full gap-2 focus:border-lime-800">
             <Icon id="Facebook" class="w-6 h-6" strokeWidth={2}></Icon>{" "}
             Sign up with Facebook
           </button>
