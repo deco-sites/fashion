0deecab9cc142d2f850fd1385b2bb9c67b0287e7
Marcos Ewbank <marcos.suarez@pivotree.com>
2023-05-21 16:20:22 +0100

    FEAT: New implementation

--- components/ui/InteractiveBanner.tsx
+++ components/ui/InteractiveBanner.tsx
@@ -4,13 +4,14 @@ import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 import type { SectionProps } from "$live/types.ts";
 import type { Context } from "deco-sites/std/packs/vtex/accounts/vtex.ts";
-import type { ImageObject } from "deco-sites/std/commerce/types.ts";
 
 export interface Props {
   /**
    * @description Title of the banner.
    */
   title?: string;
+  subtitle?: string;
+  cta?: string;
   /**
    * @description List of pins.
    */
@@ -49,7 +50,11 @@ export interface Position {
   y: number;
 }
 
-export const loader = async (props: Props, req: Request, ctx: Context) => {
+export const loader = async (
+  props: Props,
+  req: Request,
+  ctx: Context,
+) => {
   const { pins } = props;
 
   const productIds = pins.map((pin) => pin.productId);
@@ -63,11 +68,6 @@ export const loader = async (props: Props, req: Request, ctx: Context) => {
   const handleProductAndPins = pins.map((pin) => {
     const product = resProduct?.find((item) =>
       item.productID === pin.productId
-    );
-
-    console.log(
-      "🚀 ~ file: InteractiveBanner.tsx:67 ~ handleProductAndPins ~ product:",
-      product,
     );
 
     return {
@@ -84,22 +84,26 @@ export const loader = async (props: Props, req: Request, ctx: Context) => {
 
 export default function InteractiveBanner({
   title,
+  subtitle,
+  cta,
   srcMobile,
   srcDesktop,
   alt,
   pins,
 }: SectionProps<typeof loader>) {
   return (
-    <section class="container w-full px-4 md:px-0 mx-auto">
+    <section class="container w-full px-4 md:px-0 mx-auto flex flex-col lg:flex-row gap-3 rounded">
       {title &&
         (
-          <div class="py-6 md:py-0 md:pb-[40px] flex items-center mt-6">
-            <h2 class="text-lg leading-5 font-semibold uppercase">
+          <div class="flex flex-col bg-secondary h-100 text-white p-4 w-100 lg:w-2/5 rounded">
+            <h2 class="text-left text-lg leading-5 font-semibold uppercase p-2 mb-3">
               {title}
             </h2>
+            {subtitle && <p class="mb-3">{subtitle}</p>}
+            {cta && <button class="btn mt-auto">{cta}</button>}
           </div>
         )}
-      <Picture class="flex relative w">
+      <Picture class="flex relative max-h-[500px] w-100 lg:w-3/5 rounded">
         <Source
           media="(max-width: 767px)"
           src={srcMobile}
@@ -113,7 +117,7 @@ export default function InteractiveBanner({
           height={250}
         />
         <img
-          class="w-full"
+          class="w-full rounded bg-cover"
           sizes="(max-width: 640px) 100vw, 30vw"
           src={srcMobile}
           alt={alt}
@@ -126,7 +130,7 @@ export default function InteractiveBanner({
           return (
             <>
               <div
-                class="absolute cursor-pointer group/item w-5 h-5 bg-white rounded-full hover:w-auto hover:h-auto lg:hidden"
+                class="absolute cursor-pointer group/item w-5 h-5 bg-accent border-4 border-white rounded-full hover:w-auto hover:h-auto lg:hidden "
                 style={{
                   bottom: `${pin?.positionMobile?.y}%`,
                   left: `${pin?.positionMobile?.x}%`,
@@ -134,13 +138,13 @@ export default function InteractiveBanner({
               >
                 <a
                   href={product.url}
-                  class="group/edit invisible group-hover/item:visible flex whitespace-nowrap text-base-100 text-caption rounded-3xl text-center w-full justify-center items-center min-h-[36px] hover:bg-base-100 hover:text-base-content border border-base-100 p-2"
+                  class="group/edit invisible group-hover/item:visible flex whitespace-nowrap text-base-100 text-caption rounded-3xl text-center w-full justify-center items-center min-h-[36px] hover:bg-secondary border border-base-100 p-2"
                 >
                   <span>{product?.name}</span>
                 </a>
               </div>
               <div
-                class="absolute cursor-pointer group/item w-5 h-5 bg-white rounded-full hover:w-auto hover:h-auto hidden lg:flex"
+                class="absolute cursor-pointer group/item w-5 h-5 bg-accent border-4 border-white rounded-full hover:w-auto hover:h-auto hidden lg:flex"
                 style={{
                   bottom: `${pin?.positionDesktop?.y}%`,
                   left: `${pin?.positionDesktop?.x}%`,
@@ -148,7 +152,7 @@ export default function InteractiveBanner({
               >
                 <a
                   href={product.url}
-                  class="group/edit invisible group-hover/item:visible flex whitespace-nowrap text-base-100 text-caption rounded-3xl text-center w-full justify-center items-center min-h-[36px] hover:bg-base-100 hover:text-base-content border border-base-100 p-2 "
+                  class="group/edit invisible group-hover/item:visible flex whitespace-nowrap text-base-100 text-caption rounded-3xl text-center w-full justify-center items-center min-h-[36px] hover:bg-secondary border border-base-100 p-2 "
                 >
                   {product?.name}
                 </a>
