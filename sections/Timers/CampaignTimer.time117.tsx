2ed7718511017a22be79a813109675c93d70edc7
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2023-05-21 12:01:21 -0300

    adiciona opção de alinhamento

--- sections/Timers/CampaignTimer.time117.tsx
+++ sections/Timers/CampaignTimer.time117.tsx
@@ -9,31 +9,31 @@ export interface Props {
   text?: string;
   labelButton?: string;
   link?: string;
+  clockAligment?: "left" | "right";
 }
 
-function CampaignTimer({ text = 'Texto de chamada', labelButton = 'Botão', date = 'Sun May 21 2023 10:27:31 GMT-0300', link = '#' }: Props) {
-  const now = new Date().getTime();
-  const futureDate = new Date(date).getTime();
-
-  const timeleft = futureDate - now;
-
-  const msPerSecond = 1000;
-  const msPerMinute = msPerSecond * 60;
-  const msPerHour = msPerMinute * 60;
-
-  const hours = Math.floor(timeleft / msPerHour);
-  const minutes = Math.floor((timeleft % msPerHour) / msPerMinute);
-  const seconds = Math.floor((timeleft % msPerMinute) / msPerSecond);
-
+function CampaignTimer(
+  {
+    text = "Texto de chamada",
+    labelButton = "Botão",
+    date = "Sun May 21 2023 10:27:31 GMT-0300",
+    link = "#",
+    clockAligment = "right",
+  }: Props,
+) {
   return (
     <div class="bg-primary py-4">
       <div class="flex flex-wrap justify-center items-center lg:gap-16">
-        <div class="mb-4 w-full text-white lg:w-max lg:my-0">
+        <div
+          class={`mb-4 w-full text-white lg:w-max lg:my-0 ${
+            clockAligment === "right" ? "order-3" : "order-1"
+          }`}
+        >
           <p class="text-center text-sm lg:text-xl">{text}</p>
         </div>
         <Countdown date={date} />
-        <a href={link}>
-            <Button class="rounded-none">{labelButton}</Button>
+        <a href={link} class="order-4">
+          <Button class="rounded-none">{labelButton}</Button>
         </a>
       </div>
     </div>
