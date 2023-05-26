21312ea86bad29f2a3da369fb7f3d3d812b2316b
Tiago Gimenes <tlgimenes@gmail.com>
2023-05-26 09:26:32 -0300

    Make intelisense for daisy UI to work no VSCode (#342)

--- sections/DesignSystem.tsx
+++ sections/DesignSystem.tsx
@@ -7,7 +7,6 @@
 import { Color } from "https://deno.land/x/color@v0.3.0/mod.ts";
 import { useId } from "preact/hooks";
 import { Head } from "$fresh/runtime.ts";
-import { theme as defaultTheme } from "$store/tailwind.config.ts";
 import Icon from "$store/components/ui/Icon.tsx";
 
 export interface OptionalColors {
@@ -301,6 +300,29 @@ const toVariables = (t: Theme): [string, string][] => {
   });
 
   return [...colorVariables, ...miscellaneousVariables];
+};
+
+const defaultTheme = {
+  "primary": "hsl(180 100% 10%)",
+  "secondary": "hsl(0 39% 39%)",
+  "accent": "hsl(150 100% 50%)",
+  "neutral": "hsl(0 0% 20%)",
+  "base-100": "hsl(0 0% 100%)",
+  "success": "hsl(150 62% 95%)",
+  "warning": "hsl(43 100% 95%)",
+  "error": "hsl(9 100% 95%)",
+  "info": "hsl(220 100% 97%)",
+
+  "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+  "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+  "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+  "--animation-btn": "0.25s", // duration of animation when you click on button
+  "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+  "--btn-text-case": "uppercase", // set default text transform for buttons
+  "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+  "--border-btn": "1px", // border width of buttons
+  "--tab-border": "1px", // border width of tabs
+  "--tab-radius": "0.5rem", // border radius of tabs
 };
 
 /**
