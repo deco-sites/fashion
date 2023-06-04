fcd1151bcf3e09a810c7c66d62263d4ce14c3d72
gimenes <tlgimenes@gmail.com>
2023-06-04 20:14:33 -0300

    logos

--- sections/Content/Logos.tsx
+++ sections/Content/Logos.tsx
@@ -1,44 +1,49 @@
-import type { Image as ImageType } from "deco-sites/std/components/types.ts";
 import Image from "deco-sites/std/components/Image.tsx";
 import Header from "$store/components/ui/SectionHeader.tsx";
+import { useMemo } from "preact/hooks";
+import type { Image as ImageType } from "deco-sites/std/components/types.ts";
 
-export interface Img {
+export interface Image {
   image: ImageType;
-  altText?: string;
+  altText: string;
 }
 
 export interface Props {
   title?: string;
   description?: string;
-  images?: Img[];
+  images?: Image[];
   layout?: {
     headerAlignment?: "center" | "left";
   };
 }
 
-export default function Logos({
-  title,
-  description,
-  images,
-  layout,
-}: Props) {
-  const image1 = {
+const IMAGES = [
+  {
+    altText: "deco",
     image:
       "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/fe7cd8ba-c954-45d6-9282-ee7d8ca8e3c7",
-  };
-  const image2 = {
+  },
+  {
+    altText: "deco",
     image:
       "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/637e8601-6b86-4979-aa97-68013a2a60fd",
-  };
+  },
+];
 
-  const defaultImages = [];
-
-  Array(10).fill().forEach(() => {
-    defaultImages.push(image1);
-    defaultImages.push(image2);
-  });
-
-  const imgList = images && images.length > 0 ? images : defaultImages
+function Logos(props: Props) {
+  const {
+    title,
+    description,
+    images,
+    layout,
+  } = props;
+  const list = useMemo(
+    () =>
+      images && images.length > 0
+        ? images
+        : Array(20).fill(null).map((_, i) => IMAGES[i % 2]),
+    [],
+  );
 
   return (
     <div class="w-full container px-4 py-8 flex flex-col gap-12 lg:gap-16 lg:py-10 lg:px-0">
@@ -47,19 +52,19 @@ export default function Logos({
         description={description}
         alignment={layout?.headerAlignment || "center"}
       />
-      <div class="w-full text-center items-center">
-        {imgList.map((element) => (
-          <div class="w-[140px] lg:w-[200px] h-[70px] lg:h-[100px] px-2 lg:px-5 py-1 lg:py-4 inline-block align-middle">
-            <div class="flex w-full h-full items-center justify-center">
-              <Image
-                src={element.image}
-                alt={element.altText || ""}
-                class="max-w-full max-h-full"
-              />
-            </div>
-          </div>
+      <div class="grid gap-2 place-items-center grid-cols-3 sm:grid-cols-4 lg:grid-cols-5">
+        {list.map((image) => (
+          <Image
+            height={27}
+            width={75}
+            src={image.image}
+            alt={image.altText || ""}
+            class="m-8"
+          />
         ))}
       </div>
     </div>
   );
 }
+
+export default Logos;
