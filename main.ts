349c54b751bba7edb8af0d65a2a9cd9e300928e1
gimenes <tlgimenes@gmail.com>
2023-06-11 13:54:58 -0300

    wip

--- main.ts
+++ main.ts
@@ -5,12 +5,12 @@
 
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
-// import partytownPlugin from "partytown/mod.ts";
+import partytownPlugin from "partytown/mod.ts";
 import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
 
 await start($live(manifest, site), {
   plugins: [
-    // partytownPlugin(),
+    partytownPlugin(),
   ],
 });
