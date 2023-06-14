c90c0cf758625be324886e15c01a122064060786
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-06-14 15:06:52 -0300

    Fix types

--- components/footer/PaymentMethods.tsx
+++ components/footer/PaymentMethods.tsx
@@ -1,19 +1,19 @@
 import Icon from "$store/components/ui/Icon.tsx";
 
-export interface PaymentIcon {
-  label: string;
+export interface PaymentItem {
+  label: "Diners" | "Elo" | "Mastercard" | "Pix" | "Visa";
 }
 
 export default function PaymentMethods(
-  { content }: { content: { title: string; items: PaymentIcon[] } },
+  { content }: { content?: { title?: string; items?: PaymentItem[] } },
 ) {
   return (
     <>
-      {content?.items?.length > 0 && (
+      {content && content.items && content.items.length > 0 && (
         <div class="flex flex-col gap-4">
-          {content?.title && <h3 class="text-lg">{content?.title}</h3>}
+          {content.title && <h3 class="text-lg">{content.title}</h3>}
           <ul class="flex items-center gap-4 flex-wrap">
-            {content?.items.map((item) => {
+            {content.items.map((item) => {
               return (
                 <li
                   class="border"
