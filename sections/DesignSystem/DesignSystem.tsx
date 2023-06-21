bb8e7c235abe6e19fbef9176b300825d0e686f38
Augusto Barbosa <augustocbarbosa@gmail.com>
2023-06-21 11:42:19 -0300

    Fix typo

--- sections/DesignSystem/DesignSystem.tsx
+++ sections/DesignSystem/DesignSystem.tsx
@@ -353,7 +353,7 @@ function Section({
   };
   const variables = [
     ...toVariables(theme),
-    ["--font-family", fonts?.fontFamily ?? ""],
+    ["--font-family", fonts?.fontFamily ?? "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif"],
   ]
     .map(([cssVar, value]) => `${cssVar}: ${value}`)
     .join(";");
