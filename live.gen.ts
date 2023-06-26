501e093c2e6db26bd9a18c43ea50f71f5f2795ea
Marcos Candeia <marrcooos@gmail.com>
2023-06-26 08:29:54 -0300

    Use new resolver version (#400)
    * Use new resolver version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update schema
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Update deco version
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Use live 1.16.0
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- live.gen.ts
+++ live.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import { DecoManifest } from "$live/types.ts";
 import * as $$$$0 from "./routes/styles.css.ts";
 import * as $$$$1 from "./routes/_app.tsx";
@@ -335,7 +334,6 @@ const manifest = {
     "deco-sites/std/accounts/vtex.ts": i2$$3,
     "deco-sites/std/accounts/yourViews.ts": i2$$4,
   },
-  "config": config,
   "baseUrl": import.meta.url,
 };
 
