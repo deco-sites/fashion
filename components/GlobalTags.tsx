6a136c117d05f472a1d9b83b0a8e52340da84ab8
gimenes <tlgimenes@gmail.com>
2023-06-29 10:55:40 -0300

    global tags

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -1,5 +1,11 @@
 import { asset, Head } from "$fresh/runtime.ts";
 
+const gtag = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6D23BB');`;
+
 function GlobalTags() {
   return (
     <Head>
@@ -11,6 +17,11 @@ function GlobalTags() {
 
       {/* Web Manifest */}
       <link rel="manifest" href={asset("/site.webmanifest")} />
+
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{ __html: gtag }}
+      />
     </Head>
   );
 }
