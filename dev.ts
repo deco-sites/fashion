adb76733646aad04ce001aefd7e7d95a29be6ece
Marcos Candeia <marrcooos@gmail.com>
2023-07-11 12:51:13 -0300

    Use import from site json
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev.ts
+++ dev.ts
@@ -1,16 +1,12 @@
 #!/usr/bin/env -S deno run -A --watch
 import dev from "$live/dev.ts";
-import liveManifest from "$live/live.gen.ts";
-import liveStdManifest from "deco-sites/std/live.gen.ts";
 import tailwind from "deco-sites/std/tailwindv3.ts";
+import site from "./site.json" assert { type: "json" };
 
 // Start tailwind background process generation
 tailwind();
 
 // Generate manifest and boot server
 await dev(import.meta.url, "./main.ts", {
-  imports: {
-    "$live": liveManifest,
-    "deco-sites/std": liveStdManifest,
-  },
+  imports: site.imports,
 });
