fc0ea062a2319c706554573052e50cb39a24e947
gimenes <tlgimenes@gmail.com>
2023-07-20 14:54:18 -0300

    partytown

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from "$fresh/server.ts";
 import GlobalTags from "$store/components/GlobalTags.tsx";
 import Theme from "$store/sections/Theme/Theme.tsx";
+import { Head } from "$fresh/runtime.ts";
+
+const innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WQZM9B');`;
 
 function App(props: AppProps) {
   return (
@@ -13,6 +20,13 @@ function App(props: AppProps) {
 
       {/* Rest of Preact tree */}
       <props.Component />
+
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: innerHTML }}
+        />
+      </Head>
     </>
   );
 }
