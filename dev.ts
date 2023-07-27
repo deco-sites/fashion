201a82397fe668adecf156e35b91b60f7e99ed14
Tiago Gimenes <tlgimenes@gmail.com>
2023-07-27 17:42:44 -0300

    feat: Tailwind worker  (#427)

--- dev.ts
+++ dev.ts
@@ -1,10 +1,6 @@
 #!/usr/bin/env -S deno run -A --watch
 import dev from "$live/dev.ts";
-import tailwind from "deco-sites/std/tailwindv3.ts";
 import site from "./site.json" assert { type: "json" };
-
-// Start tailwind background process generation
-tailwind();
 
 // Generate manifest and boot server
 await dev(import.meta.url, "./main.ts", site);
