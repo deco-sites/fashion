346595f8d8b0314ca20ee1ba3a94b5cfa8921f1c
gimenes <tlgimenes@gmail.com>
2023-07-27 22:36:47 -0300

    imagemagic

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
 import { plugin as tailwindPlugin } from "deco-sites/std/plugins/tailwind/mod.ts";
+import { plugin as imagePlugin } from "deco-sites/std/plugins/image/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
 import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
@@ -14,5 +15,6 @@ await start($live(manifest, site), {
   plugins: [
     partytownPlugin(),
     tailwindPlugin,
+    imagePlugin,
   ],
 });
