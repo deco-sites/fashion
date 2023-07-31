544398d17bf8cca4d5ad3f907d7ad06bc27cc97a
gimenes <tlgimenes@gmail.com>
2023-07-31 13:58:26 -0300

    wip

--- main.ts
+++ main.ts
@@ -5,8 +5,7 @@
 
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
-import { plugin as tailwindPlugin } from "deco-sites/std/plugins/tailwind/mod.ts";
-import { plugin as imagePlugin } from "deco-sites/std/plugins/image/mod.ts";
+import { plugin as deco } from "deco-sites/std/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
 import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
@@ -14,7 +13,6 @@ import site from "./site.json" assert { type: "json" };
 await start($live(manifest, site), {
   plugins: [
     partytownPlugin(),
-    tailwindPlugin,
-    imagePlugin,
+    deco,
   ],
 });
