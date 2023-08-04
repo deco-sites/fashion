6cf056fb285836dc1e690840efcf1fae35e145b0
Tiago Gimenes <tlgimenes@gmail.com>
2023-08-04 12:13:37 -0300

    feat: Same domain images (#431)

--- live.gen.ts
+++ live.gen.ts
@@ -147,7 +147,8 @@ import * as i2$$$26 from "deco-sites/std/loaders/vtex/proxy.ts";
 import * as i2$$$27 from "deco-sites/std/loaders/vtex/user.ts";
 import * as i2$$$28 from "deco-sites/std/loaders/vtex/wishlist.ts";
 import * as i2$$$29 from "deco-sites/std/loaders/x/font.ts";
-import * as i2$$$30 from "deco-sites/std/loaders/x/redirectsFromCsv.ts";
+import * as i2$$$30 from "deco-sites/std/loaders/x/image.ts";
+import * as i2$$$31 from "deco-sites/std/loaders/x/redirectsFromCsv.ts";
 import * as i2$$$$7 from "deco-sites/std/handlers/sitemap.ts";
 import * as i2$$$$8 from "deco-sites/std/handlers/vtex/sitemap.ts";
 import * as i2$$$$$5 from "deco-sites/std/sections/Analytics.tsx";
@@ -290,7 +291,8 @@ const manifest = {
     "deco-sites/std/loaders/vtex/user.ts": i2$$$27,
     "deco-sites/std/loaders/vtex/wishlist.ts": i2$$$28,
     "deco-sites/std/loaders/x/font.ts": i2$$$29,
-    "deco-sites/std/loaders/x/redirectsFromCsv.ts": i2$$$30,
+    "deco-sites/std/loaders/x/image.ts": i2$$$30,
+    "deco-sites/std/loaders/x/redirectsFromCsv.ts": i2$$$31,
   },
   "handlers": {
     "$live/handlers/devPage.ts": i2$$$$0,
