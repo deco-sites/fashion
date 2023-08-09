e60ae4dd26fd8d33a0578f99fd158c03ce772e32
Marcos Candeia <marrcooos@gmail.com>
2023-08-09 13:42:33 -0300

    Use deco plugin (#435)
    * Use deco plugin
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Regen cache
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@
 
 import { start } from "$fresh/server.ts";
 import { $live } from "$live/mod.ts";
-import { plugin as tailwindPlugin } from "deco-sites/std/plugins/tailwind/mod.ts";
+import plugins from "deco-sites/std/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
 import manifest from "./live.gen.ts";
 import site from "./site.json" assert { type: "json" };
 
 await start($live(manifest, site), {
   plugins: [
+    ...plugins(),
     partytownPlugin(),
-    tailwindPlugin,
   ],
 });
