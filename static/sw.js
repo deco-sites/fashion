48997c30b6adce0ae8845c6f71e71f2a1592cf4e
gimenes <tlgimenes@gmail.com>
2023-08-10 17:26:44 -0300

    add workbox offline page

--- static/sw.js
+++ static/sw.js
@@ -2,9 +2,6 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.setConfig({
-  debug: false,
-});
-
+workbox.setConfig({ debug: false });
 workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
 workbox.recipes.offlineFallback({ pageFallback: '/offline' });
