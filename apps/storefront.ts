59b3783ddf62dd30a5c4181df64a5702a6d1c8e9
gimenes <tlgimenes@gmail.com>
2023-08-18 00:17:36 -0300

    wip

--- apps/storefront.ts
+++ apps/storefront.ts
@@ -2,34 +2,38 @@ import { Section } from "$live/blocks/section.ts";
 import { App } from "$live/mod.ts";
 import vnda, { Props as VNDAProps } from "apps/vnda/mod.ts";
 import vtex, { Props as VTEXProps } from "apps/vtex/mod.ts";
+import shopify, { Props as ShopifyProps } from "apps/shopify/mod.ts";
 import website, { Props as WebsiteProps } from "apps/website/mod.ts";
 import type { ComponentProps } from "preact";
 import manifest, { Manifest } from "../manifest.gen.ts";
 
 export type State = WebsiteProps & {
   theme: Section;
-  commerce: VNDAProps | VTEXProps;
+  commerce: VNDAProps | VTEXProps | ShopifyProps;
 };
 
 type WebsiteApp = ReturnType<typeof website>;
-type CommerceApp = ReturnType<typeof vnda> | ReturnType<typeof vtex>;
+type CommerceApp =
+  | ReturnType<typeof vnda>
+  | ReturnType<typeof vtex>
+  | ReturnType<typeof shopify>;
 
-const PLATFORMS = { vnda, vtex };
+const PLATFORMS = { vnda, vtex, shopify };
 
 // Uncomment for testing and specific platform
-const DEBUG_TEST: null | keyof typeof PLATFORMS = "vnda";
+const DEBUG_TEST: null | keyof typeof PLATFORMS = null; // "shopify";
 
 export let _platform: null | keyof typeof PLATFORMS = null;
 
 export default function Storefront(
   state: State,
 ): App<Manifest, State, [WebsiteApp, CommerceApp]> {
-  _platform = state.commerce.platform;
-
-  if (!_platform) throw new Error("Missing platform");
-  if (DEBUG_TEST !== _platform) {
+  if (!state.commerce.platform) throw new Error("Missing platform");
+  if (DEBUG_TEST !== state.commerce.platform) {
     return { state, manifest, dependencies: [] as any };
   }
+
+  _platform = state.commerce.platform;
 
   // @ts-expect-error Trust me, I'm an engineer
   const ecommerce = PLATFORMS[_platform]?.(state.commerce);
